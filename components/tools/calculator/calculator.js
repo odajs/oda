32feeefeda10daf18a0171a8b9c1864725f82d13
ilysaz <ilysaz@gmail.com>
2021-11-11 16:30:03 +0300

    AutoCommit: 63114

--- components/tools/calculator/calculator.js
+++ components/tools/calculator/calculator.js
@@ -14,7 +14,7 @@ ODA({is: 'oda-calculator', imports: '@oda/button',
         </style>
         <div class="border vertical" style="margin-bottom: 16px; text-align: right">
             <span style="font-size: small" class="dimmed">{{result}}</span>
-            <span style="font-size: large">{{expression || 0}}
+            <span style="font-size: large">{{expression || value}}
                 <span disabled>{{predicate}}</span>
             </span>
         </div>
@@ -27,17 +27,6 @@ ODA({is: 'oda-calculator', imports: '@oda/button',
             return i.predicate;
         }).join('');
     },
-    predicates: [],
-    hostAttributes: {
-        tabindex: 1
-    },
-    keyBindings: {
-        p () {
-            alert('HI')
-        }
-    },
-    result: '0',
-    value: 0, // переменная для представления выражения, которое было записано ранее
     get expression(){
         return this.stack.map(i=>{
             return (i.name || i.label);
@@ -46,8 +35,20 @@ ODA({is: 'oda-calculator', imports: '@oda/button',
     data: {
         rows:[]
     },
+    predicates: [],
     stack: [],
-    tap(e){
+    signs: ['+', '-', '*', '/'],
+    result: '0',
+    value: 0, 
+    hostAttributes: {
+        tabindex: 1
+    },
+    keyBindings: {
+        p () {
+            alert('HI')
+        }
+    },
+    tap (e) {
         const model = e.target.model;
         switch (model.command){
             case 'calc':
@@ -55,15 +56,23 @@ ODA({is: 'oda-calculator', imports: '@oda/button',
             case 'back':
                 return this[model.command]();
         }
-        if (this.predicates[0]?.predicate === (model.name || model.label)){
+        if (this.predicates[0]?.predicate === (model.name || model.label)){ // checking closing brackets
             this.predicates.shift();
         }
-        this.stack.push(model)
+        if (this.expression === '0' && model.label === 0) { 
+            return
+        } else if (this.canBeDeleted(model)) { 
+            this.stack.splice(0, 1, model);
+        } else if (this.canBeReplaced(model)) {
+            this.stack.splice(-1, 1, model);
+            this.expression = undefined; // activation of reactivity 
+        } else {
+            this.stack.push(model);
+        } 
         if (model.predicate) 
-            this.predicates.unshift(model);
-            this.expression = undefined;
-            this.predicate = undefined;
-        
+            this.predicates.unshift({label: model.predicate, predicate: model.predicate});
+            this.expression = undefined; 
+            this.predicate = undefined; 
     },
     calc () {
         this.stack.push(...this.predicates);
@@ -79,16 +88,34 @@ ODA({is: 'oda-calculator', imports: '@oda/button',
         catch (e){
             console.error(e)
         }
-        this.stack = [];
-
+        this.stack = [{label: this.value, result: true}];
     },
-    clear(){
-        this.stack = [];
+    clear () {
+        this.stack = [{label: 0}];
         this.predicate = [];
         this.value = 0;
         this.result = '0';
     },
-    back(){
-
+    back () {
+        if (this.stack[this.stack.length-1]?.predicate === this.predicates[this.predicates.length - 1]?.predicate){ 
+            this.predicates.shift();
+            this.predicate = undefined;
+        }
+        if (this.stack.length === 1) {
+            this.stack.splice(-1, 1, {label: 0})
+            this.expression = undefined;
+        } else {
+            this.stack.pop();
+            this.expression = undefined;
+            this.predicate = undefined;
+        }
+    },
+    // checking if the value in the output line can be deleted
+    canBeDeleted (model) {
+        return (this.expression === '0' || this.stack[this.stack.length-1]?.result) && typeof model.label === 'number'
+    },
+    // checking the possibility of replacing the mathematical sign
+    canBeReplaced (model) {
+        return this.signs.some((e) => e === this.stack[this.stack.length-1]?.label) && this.signs.some((e) => e === model.label)
     }
 })
