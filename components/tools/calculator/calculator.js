a6c565be4be055259b999debc926cb3aa976171e
ilysaz <ilysaz@gmail.com>
2021-11-12 15:00:03 +0300

    AutoCommit: 63171

--- components/tools/calculator/calculator.js
+++ components/tools/calculator/calculator.js
@@ -14,7 +14,7 @@ ODA({is: 'oda-calculator', imports: '@oda/button',
         </style>
         <div class="border vertical" style="margin-bottom: 16px; text-align: right">
             <span style="font-size: small" class="dimmed">{{result}}</span>
-            <span style="font-size: large">{{expression || value}}
+            <span style="font-size: large">{{error || expression || value}}
                 <span disabled>{{predicate}}</span>
             </span>
         </div>
@@ -40,6 +40,7 @@ ODA({is: 'oda-calculator', imports: '@oda/button',
     signs: ['+', '-', '*', '/'],
     result: '0',
     value: 0, 
+    error: '',
     hostAttributes: {
         tabindex: 1
     },
@@ -72,14 +73,17 @@ ODA({is: 'oda-calculator', imports: '@oda/button',
         } 
         if (model.label === '(') {
             this.stack.push(model);
-            let numbersBeforeBrackets = this.getExpression().match(/\d(?=\()/);
-            numbersBeforeBrackets = numbersBeforeBrackets[0].replace(numbersBeforeBrackets[0], `${numbersBeforeBrackets[0]}*`);
-            return this.stack[this.stack.length-1].expr = numbersBeforeBrackets
-        }
-        if ((model.name || model.label) === '.') {
+            if (this.getExpression().match(/\d+\.?\d*(?=\()/)) { // if exist the number after which comes the bracket, add '*' in front of the bracket
+                this.stack[this.stack.length-1] = {label: model.label, expr: '*' + model.label};
+                this.getReactivity();
+            }
+        } else if (model.label === this.stack[this.stack.length-1].predicate) {
+            return
+        } else if ((model.name || model.label) === '.') {
             const arr = this.getExpression().match(/\d+\.?(\d*)?/g); // get an array of all entered numbers for further checks
             arr[(arr.length - 1)].match(/\./) || this.getExpression().match(/\D$/) ? false : this.stack.push(model);
         } else if (this.canBeDeleted(model)) { 
+            this.error = '';
             this.stack.splice(0, 1, model);
         } else if (this.canBeReplaced(model)) {
             this.stack.splice(-1, 1, model);
@@ -95,14 +99,18 @@ ODA({is: 'oda-calculator', imports: '@oda/button',
         this.stack.push(...this.predicates);
         this.getReactivity();
         const expr = this.getExpression();
-        expr.match(/\d\(/g);
         this.predicates = [];
-        try{
+        try {
+            if (expr.match(/\D$/)) { // if the expression ends with a mathematical sign, do not count
+                return
+            }
             this.value = (new Function([], `with (this) {return ${expr}}`)).call(this);
             this.result = this.expression + ' =';
         }
-        catch (e){
-            console.error(e);
+        catch (e) {
+            this.error = 'Error';
+            this.stack = [];
+            console.log(e);
         }
         this.stack = [{label: this.value, result: true}]; // push the result to the stack
     },
@@ -111,6 +119,7 @@ ODA({is: 'oda-calculator', imports: '@oda/button',
         this.predicates = [];
         this.value = 0;
         this.result = '0';
+        this.error = '';
     },
     back () {
         if (this.stack[this.stack.length-1]?.predicate === this.predicates[this.predicates.length - 1]?.predicate){ 
@@ -128,7 +137,7 @@ ODA({is: 'oda-calculator', imports: '@oda/button',
     // checking if the value in the output line can be deleted
     canBeDeleted (model) {
         return (this.expression === '0' || this.stack[this.stack.length-1]?.result) 
-                && (typeof model.label === 'number' || model.label === 'sin' || 'atan')
+                && (typeof model.label === 'number' || model.label === ('sin' || 'atan'))
     },
     // checking the possibility of replacing the mathematical sign
     canBeReplaced (model) {
@@ -142,7 +151,7 @@ ODA({is: 'oda-calculator', imports: '@oda/button',
     // get the expression as a string
     getExpression () {
         const expr = this.stack.map(i=>{
-            return (i.expr || i.name || i.label);
+            return (i.expr || i.label);
         }).join('');
         return expr
     }
