411ac21f5281dcd3ec34d8e7a5dca4f90a118d56
ilysaz <ilysaz@gmail.com>
2021-11-30 10:30:04 +0300

    AutoCommit: 63948

---
+++ components/viewers/leaflet/leaflet.js
@@ -0,0 +1,64 @@
+import * as L from './lib/leaflet-src.esm.js';
+
+const iconRetinaUrl = './lib/images/marker-icon-2x.png';
+const iconUrl = './lib/images/marker-icon.png';
+const shadowUrl = './lib/images/marker-shadow.png';
+const iconDefault = L.icon({
+    iconRetinaUrl, iconUrl, shadowUrl,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    tooltipAnchor: [16, -28],
+    shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = iconDefault;
+
+ODA({ is: 'oda-leaflet', template: `
+        <style>
+            :host {
+                display:block;
+                height: 100%;
+            }
+        </style>
+        <link rel="stylesheet" href="./lib/leaflet.css">
+        <div id="mapid" style="height: 100%"></div>
+    `,
+    props: {
+        latitude: { type: Number },
+        longitude: { type: Number },
+        zoom: 13,
+        minZoom: 3,
+        markers: [],
+        polygons: [],
+        circles: []
+    },
+    attached() {
+        this._map = L.map(this.$('#mapid'));
+        this._map.setView([this.latitude, this.longitude], this.zoom);
+        let urlTemplate = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
+        this._map.addLayer(L.tileLayer(urlTemplate, { minZoom: this.minZoom }));
+        this.markers.forEach(i => {
+            if (i.latitude && i.longitude) {
+                let el = L.marker([i.latitude, i.longitude]).addTo(this._map);
+                if (i.bindPopup) el.bindPopup(i.bindPopup);
+            }
+        });
+        this.polygons.forEach(i => {
+            if (i.polygons?.length) {
+                let el = L.polygon([i.polygons]).addTo(this._map);
+                if (i.bindPopup) el.bindPopup(i.bindPopup);
+            }
+        });
+        this.circles.forEach(i => {
+            if (i.latitude && i.longitude) {
+                let el = L.circle([i.latitude, i.longitude], { ...i.args }).addTo(this._map);
+                if (i.bindPopup) el.bindPopup(i.bindPopup);
+            }
+        });
+    },
+    observers: [
+        function _observers(latitude, longitude, zoom) {
+            this._map.setView([latitude, longitude], zoom);
+        }
+    ]
+})
