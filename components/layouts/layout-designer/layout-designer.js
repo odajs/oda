a5e55f8b35694e1d29c437b19de4a2981b4afa82
ilysaz <ilysaz@gmail.com>
2021-12-07 12:00:04 +0300

    AutoCommit: 64247

--- components/layouts/layout-designer/layout-designer.js
+++ components/layouts/layout-designer/layout-designer.js
@@ -1,5 +1,5 @@
-ODA({is:'oda-layout-designer',
-    template:`
+ODA({ is: 'oda-layout-designer',
+    template: `
         <style>
             :host{
                 overflow-x: hidden;
@@ -12,7 +12,7 @@ ODA({is:'oda-layout-designer',
 <!--        <div ~if="designMode" :slot="designMode?'left-panel':'?'">дерево</div>-->
     `,
     data: null,
-    props:{
+    props: {
         designMode: false,
         keys: ''
     },
@@ -23,7 +23,7 @@ ODA({is:'oda-layout-designer',
     structureTemplate: 'oda-layout-designer-structure',
 
 })
-ODA({is:'oda-layout-designer-structure',
+ODA({ is: 'oda-layout-designer-structure',
     template: /*html*/`
         <style>
             :host{
@@ -40,20 +40,20 @@ ODA({is:'oda-layout-designer-structure',
     `,
     layout: null,
     iconSize: 32,
-    get $saveKey(){
+    get $saveKey() {
         return this.layout.name;
     },
-    props:{
-        settings:{
-            default: {acts:[]},
+    props: {
+        settings: {
+            default: { acts: [] },
             save: true
         }
     }
 
 })
 
-ODA({is:'oda-layout-designer-group', imports: '@oda/button',
-    template:`
+ODA({ is: 'oda-layout-designer-group', imports: '@oda/button',
+    template: `
         <style>
             :host{
                 @apply --horizontal;
@@ -82,16 +82,16 @@ ODA({is:'oda-layout-designer-group', imports: '@oda/button',
         </div>
         <oda-button :icon-size @tap.stop="addTab" ~if="designMode" icon="icons:add"></oda-button>
     `,
-    ungroup(e){
+    ungroup(e) {
 
     },
-    addTab(){
+    addTab() {
         this.layout.addTab();
     }
 })
 
-ODA({is:'oda-layout-designer-group-structure',
-    template:`
+ODA({ is: 'oda-layout-designer-group-structure',
+    template: `
         <style>
             :host{
                 @apply --horizontal;
@@ -110,7 +110,7 @@ ODA({is:'oda-layout-designer-group-structure',
 })
 
 
-ODA({is:'oda-layout-designer-container', imports: '@oda/icon, @oda/menu',
+ODA({ is: 'oda-layout-designer-container', imports: '@oda/icon, @oda/menu',
     template: /*html*/`
         <style>
             :host{
@@ -123,6 +123,7 @@ ODA({is:'oda-layout-designer-container', imports: '@oda/icon, @oda/menu',
                 flex: {{width?'0 0 auto':'1000000000000000000000000000000 1 auto'}};
                 /*flex-basis: auto;*/
                 cursor: {{designMode ? 'pointer' : ''}};
+                position: relative;
             }
             .structure{
                 margin-left: {{layout?.isGroup?0:iconSize}}px;
@@ -142,15 +143,48 @@ ODA({is:'oda-layout-designer-container', imports: '@oda/icon, @oda/menu',
                 @apply --focused;
                 background-color: lightyellow;
             }
+            .drag-to-left:after {
+                box-shadow: inset 4px 0 0 0 var(--success-color);
+            }
+            .drag-to-right:after {
+                box-shadow: inset -4px 0 0 0 var(--success-color);
+            }
+            .drag-to-top:after {
+                box-shadow: inset 0 4px 0 0 var(--success-color);
+            }
+            .drag-to-bottom:after {
+                box-shadow: inset 0 -4px 0 0 var(--success-color);
+            }
+            .drag-to:after {
+                text-align: center;
+                font-size: smaller;
+                font-weight: bolder;
+                content: attr(capture);
+                pointer-events: none;
+                position: absolute;
+                left: 0;
+                top: 0;
+                right: 0;
+                bottom: 0;
+                background-color: rgba(0,255,0,.3);
+            }
+            .drag-to-error:after {
+                content: "not allow drop";
+                pointer-events: none;
+                background-color: rgba(255,0,0,.3) !important;
+            }
         </style>
         <div ~if="designMode" ~is="designMode?'style':'div'">
              :host{
                 outline: 1px dashed blue;
             }
         </div>
-        <div class="horizontal flex" style="align-items: end; overflow: hidden" @pointerdown="_tap" ~class="{selected: designMode && data?.selection?.has(layout)}">
+        <div class="horizontal flex" style="align-items: end; overflow: hidden" @pointerdown="_tap" :draggable
+                ~class="{selected: designMode && data?.selection?.includes(layout), 'drag-to':layout?.dragTo, [layout?.dragTo]:layout?.dragTo}">
             <oda-icon style="cursor: pointer;" :icon-size :icon="hasChildren?(layout?.$expanded?'icons:chevron-right:90':'icons:chevron-right'):''" @tap="expand()"></oda-icon>
-            <div class="vertical flex" style="overflow: hidden;"  :disabled="designMode && !layout?.isGroup" ~class="{group:layout.isGroup}" ~style="{alignItems: (width && !layout?.type)?'center':''}">
+            <div class="vertical flex" style="overflow: hidden;"  :disabled="designMode && !layout?.isGroup" 
+                    ~class="{group:layout.isGroup}" 
+                    ~style="{alignItems: (width && !layout?.type)?'center':''}">
 <!--            <div class="vertical flex" style="overflow: hidden;"  :disabled="designMode && !layout?.isGroup" ~class="{group:layout.isGroup}" ~style="{flexDirection: labelPos==='top'?'column':'row', textAlign:  labelPos ==='top'?'start':'end'}">-->
                 <label ~if="showLabel" class="no-flex">{{layout?.label}}</label>
                 <div class="flex" ~is="layout?.$template || editTemplate" :layout ::width></div>
@@ -159,31 +193,43 @@ ODA({is:'oda-layout-designer-container', imports: '@oda/icon, @oda/menu',
         <div ~if="hasChildren && layout?.$expanded" ~is="layout?.$structure || structureTemplate" :layout class="flex structure" style="margin-bottom: 16px; margin-right: 1px;"></div>
     `,
     width: undefined,
-    get hasChildren(){
+    get hasChildren() {
         return this.layout?.items?.length;
     },
-    expand(){
+    expand() {
         this.layout && (this.layout.$expanded = !this.layout.$expanded);
     },
-    listeners:{
-        async contextmenu(e){
+    listeners: {
+        async contextmenu(e) {
             e.preventDefault();
             e.stopPropagation();
             if (!this.designMode) return;
-            const res = await ODA.showDropdown('oda-menu', {items: [{label: 'grouping', run:()=>{
-                const action = {acts: "grouping", target: this.layout.id}
-                this.settings.acts.push(action);
-                action.id = this.toGroup(action);
-            }}, {label: 'hide'}]},{title: e.target.layout?.label});
+            const res = await ODA.showDropdown('oda-menu', {
+                items: [{
+                    label: 'grouping', run: () => {
+                        const action = { acts: "grouping", target: this.layout.id }
+                        this.settings.acts.push(action);
+                        action.id = this.toGroup(action);
+                    }
+                }, { label: 'hide' }]
+            }, { title: e.target.layout?.label });
             res.focusedItem.run.call(this)
-        }
+        },
+        'dragstart': '_dragstart',
+        'dragend': '_dragend',
+        'dragover': '_dragover',
+        'dragleave': '_dragleave',
+        'drop': '_dragdrop',
     },
     labelPos: 'top',
-    get showLabel(){
+    get showLabel() {
         return !this.layout.isGroup;
     },
     layout: null,
-    toGroup(){
+    get draggable() {
+        return this.layout && this.designMode ? 'true' : 'false';
+    },
+    toGroup() {
         return this.layout.toGroup();
     },
     _tap(e) {
@@ -195,9 +241,59 @@ ODA({is:'oda-layout-designer-container', imports: '@oda/icon, @oda/menu',
         }
         this.data.focused = this.layout;
         this.data.selection.add(this.layout);
+    },
+    _dragstart(e) {
+        e.stopPropagation();
+        this.data.dragItem = this.layout;
+        e.dataTransfer.setDragImage((this.data.selection && this.data.selection.includes(this.data.dragItem) && this.data.selection.length) > 1 ? img3 : img, -20, 7);
+    },
+    _dragend(e) {
+        this._clearDragTo();
+    },
+    _dragover(e) {
+        e.stopPropagation();
+        this._clearDragTo();
+        if (this.data?.dragItem) {
+            this.layout.dragTo = 'drag-to-error';
+            if ((this.data.selection && this.data.selection.includes(this.data.dragItem) && this.isSelected) || this.data.dragItem === this.layout) return;
+            if (this.data.dragItem.owner !== this.layout.owner) return;
+            this._clearDragTo();
+            e.preventDefault();
+            let to = '',
+                x = e.layerX,
+                y = e.layerY,
+                w = e.target.offsetWidth,
+                h = e.target.offsetHeight;
+            x = (x - w / 2) / w * 2;
+            y = (y - h / 2) / h * 2;
+            if (Math.abs(x) > Math.abs(y))
+                to = x < 0 ? 'left' : 'right';
+            else
+                to = y < 0 ? 'top' : 'bottom';
+            this.data.action = 'move';
+            this.data.to = to;
+            this.layout.dragTo = 'drag-to-' + to;
+            this.data.targetItem = this.layout;
+        }
+    },
+    _dragleave(e) {
+        this._clearDragTo();
+    },
+    _dragdrop(e) {
+        e.stopPropagation();
+        this._clearDragTo();
+    },
+    _clearDragTo() {
+        this.capture = '';
+        this.layout.dragTo = '';
+        this.layout.owner.items.forEach(i => i.dragTo = '');
+        if (this.data.last) this.data.last.layout.dragTo = '';
+        if (this.domHost && this.domHost.layout) this.domHost.layout.dragTo = '';
+        this.render();
     }
 })
-KERNEL({ is: 'Layout',
+KERNEL({
+    is: 'Layout',
     ctor(data, key = 'items', owner) {
         this.data = data || {};
         this.key = key;
@@ -208,8 +304,8 @@ KERNEL({ is: 'Layout',
     $expanded: false,
     get items() {
         const items = this.data?.[this.key];
-        if (items?.then){
-            return items.then(items =>{
+        if (items?.then) {
+            return items.then(items => {
                 this.items = items.map(i => new Layout(i, this.key, this))
             })
         }
@@ -224,19 +320,19 @@ KERNEL({ is: 'Layout',
     get label() {
         return this.data?.label || this.name;
     },
-    get $template(){
-        return this.isGroup?'oda-layout-designer-group':'';
+    get $template() {
+        return this.isGroup ? 'oda-layout-designer-group' : '';
     },
-    get $structure(){
-        return this.isGroup?'oda-layout-designer-group-structure':'';
+    get $structure() {
+        return this.isGroup ? 'oda-layout-designer-group-structure' : '';
     },
     get isGroup() {
         return this.type === "group";
     },
-    toGroup(){
+    toGroup() {
         const myIdx = this.owner.items.indexOf(this);
-        const group = new Layout({label: `Group for ${this.label}`}, this.key, this);
-        const block = new Layout({label: `Group for ${this.label}`}, this.key, group);
+        const group = new Layout({ label: `Group for ${this.label}` }, this.key, this);
+        const block = new Layout({ label: `Group for ${this.label}` }, this.key, group);
         group.type = 'group';
         group.width = 0;
         group.items = [block];
@@ -245,9 +341,15 @@ KERNEL({ is: 'Layout',
         block.items = [this];
         this.owner.items.splice(myIdx, 1, group);
     },
-    addTab(){
-        const tab = new Layout({label: `Tab ${this.items.length+1}`}, this.key, this);
+    addTab() {
+        const tab = new Layout({ label: `Tab ${this.items.length + 1}` }, this.key, this);
         this.items.push(tab)
         this.$focused = tab;
     }
 })
+
+const getUUID = function b(a) { return a ? (a ^ Math.random() * 16 >> a / 4).toString(16) : ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, b) };
+const img = new Image();
+img.src = `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAQCAYAAABQrvyxAAAACXBIWXMAAAsSAAALEgHS3X78AAAAa0lEQVRIiWPU6v91RFv4jwIv+78/DEMIfP7JxHL1LcsDFpDjJ7p8kB5KjoeB/D0CDExDLeSRAcjtTIPHOeSBUQ8MNBj1wECDUQ8MNBj1wECDUQ8MNGACteqGquNBbgc3SUGtuiHZnH7L8gAAtichl6hs6rYAAAAASUVORK5CYII=`;
+const img3 = new Image();
+img3.src = `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADQAAAAUCAYAAADC1B7dAAAACXBIWXMAAAsSAAALEgHS3X78AAAA4klEQVRYhWPU6v91RFv4jwIv+78/DEMIfP7JxHL1LcuDqwWsNsiuZgF5ZqLLB+mh5BkYyN8jwMDAwIoixjTUYgYZ8LL/Ew9b/P2J9oTfR2DCTIPCZWQCQfb/LKDUBUplMBNYhponsAFYTIHy1JCOIRhAjqlh4SEYAJUHw8pDDEO9UMAGRj002MGohwY7GH4eArVaB4E7yAIffzFiaAM3wUGtVlDzAVTjDgmfQD3z6SdmAmOB9CdYGUBtoRbbodmNQI4peIwMl5hi/P//P4oCUEwN4Q7fU4yYQIqpodclf8vyAAC+a17T0iNSKwAAAABJRU5ErkJggg==`;
