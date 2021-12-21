7f28e8d8d6b0f8bfac9202a678e06a7137108637
ilysaz <ilysaz@gmail.com>
2021-12-21 12:30:03 +0300

    AutoCommit: 64704

--- components/inputs/nunder-input-new/number-input-new.js
+++ components/inputs/nunder-input-new/number-input-new.js
@@ -8,23 +8,57 @@ ODA({is:'oda-number',
                 text-align: right;
                 width: 200px;
                 margin: 16px;
+                cursor: text;
             }
             div{
                 align-items: center;
             }
         </style>
         <span>value: {{value}}</span>
-        <span>mask:<input ::value="mask"></span>
         <div class="horizontal">
-            <input @input="onInput" :value="maskedValue">
+            <input @keydown="onKeyDown" :value="inputValue">
         </div>
     `,
-    mask: '# 0.000',
-    value: 0,
-    get maskedValue () {
-
+    precision: 3,
+    thousandSeparator: ' ',
+    decimalSeparator: '.',
+    value: 5476.547576,
+    get inputValue () {
+        return this.value.toLocaleString().replace(',', this.decimalSeparator);
+    },
+    onKeyDown(e){
+        const char = e.key;
+        const start = e.target.selectionStart
+        const end = e.target.selectionEnd;
+        const length = this.inputValue.length;
+        const decimalPos = this.inputValue.indexOf(this.decimalSeparator);
+        const value = this.inputValue.replace(/\s/g, '');
+        this.input = e.target;
+        e.preventDefault();
+        switch (char){
+            case 'Backspace':{
+                this.value = +value.slice(0,-1)
+            } break;
+        }
     },
     async onInput (e) {
+        const char = e.data === ',' ? '.' : e.data,
+            start = e.target.selectionStart,
+            end = e.target.selectionEnd;
+        this.input = e.target;
+        switch (e.inputType) {
+            case 'insertText': {
 
+            } break;
+            case 'insertFromPaste': {
+
+            } break;
+            case 'deleteContentBackward': {
+
+            } break;
+            case 'deleteContentForward': {
+
+            } break;
+        }
     }
 })
