7850c273b17109e40f6c7cfdeba9bbd291a9033f
ilysaz <ilysaz@gmail.com>
2022-01-24 15:30:05 +0300

    AutoCommit: 65601

--- tools/utils/activation/activation.js
+++ tools/utils/activation/activation.js
@@ -1,6 +1,6 @@
-function sigmoid(t) {
+export function sigmoid(t) {
     return 1/(1+Math.pow(Math.E, -t));
 }
-function sigmoidDer(val) {
+export function sigmoidDer(val) {
     return val * (1 - val);
 }
