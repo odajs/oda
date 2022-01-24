09d416ebf957bece912d10b4f674082340d428fa
ilysaz <ilysaz@gmail.com>
2022-01-24 09:30:04 +0300

    AutoCommit: 65571

--- components/tools/calculator2/$info/props/_info.js
+++
@@ -1,4 +0,0 @@
-export default [
-    { label: 'accuracy', src: './$info/props/accuracy.md', title: 'Точность результата', color: 'green' },
-    { label: 'buttonHeight', src: './$info/props/button-height.md', title: 'Высота кнопок калькулятора', color: 'green' },
-]
--- components/tools/template/$info/demo/_info.js
+++
@@ -1,4 +0,0 @@
-export default [
-    { label: 'demo1', src: './$info/demo/demo1.html', title: 'demo1' },
-    { label: 'demo2', src: './$info/demo/demo2.html', title: 'demo2' },
-]
