d803d3efefbf16863b23012a63338d563af78de0
ilysaz <ilysaz@gmail.com>
2022-02-15 13:00:04 +0300

    AutoCommit: 65995

--- tools/jupyter/jupyter.js
+++ tools/jupyter/jupyter.js
@@ -26,7 +26,8 @@ ODA({ is: 'oda-jupyter',
                 this.editedCell = undefined
             }
         },
-        showBorder: false // show cell border for mode is not readOnly
+        showBorder: false, // show cell border for mode is not readOnly
+        extMode: false
     },
     notebook: {},
     editedCell: undefined,
@@ -153,7 +154,7 @@ ODA({ is: 'oda-jupyter-cell-addbutton', imports: '@oda/button, @tools/containers
     iconSize: 14,
     cell: {},
     async showCellViews(view) {
-        const res = await ODA.showDropdown('oda-jupyter-list-views', { cell: this.cell, notebook: this.notebook, position: this.position, view }, {parent: this.$('oda-button')});
+        const res = await ODA.showDropdown('oda-jupyter-list-views', { cell: this.cell, notebook: this.notebook, position: this.position, view, extMode: this.extMode }, {parent: this.$('oda-button')});
         if (res && view === 'add') this.editedCell = undefined;
     }
 })
@@ -177,11 +178,20 @@ ODA({ is: 'oda-jupyter-list-views', imports: '@oda/button',
     cell: {},
     notebook: {},
     get cellViews() { 
-        return [
+        let views = [
             { cell_type: 'markdown', cell_extType: 'md', color: '#F7630C', source: '🟠... ', label: 'md' },
             { cell_type: 'html', cell_extType: 'html', color: '#16C60C', source: '🟢... ', label: 'html' },
             { cell_type: 'code', cell_extType: 'code', color: '#0078D7', source: '🔵... ', label: 'code' },
         ]
+        if (this.extMode) {
+            views = [...views, ...[
+                { cell_type: 'html', cell_extType: 'odant-help-info', color: '#e1ad21', source: '🟡... ', label: 'odant-help-info' },
+                { cell_type: 'html', cell_extType: 'odant-help-fields', color: '#886CE4', source: '🟣... ', label: 'odant-help-fields' },
+                { cell_type: 'html', cell_extType: 'odant-help-methods', color: '#8E562E', source: '🟤... ', label: 'odant-help-methods' },
+                { cell_type: 'html', cell_extType: 'odant-help-price', color: '#E81224', source: '🔴... ', label: 'odant-help-price' },
+            ]]
+        }
+        return views;
     },
     addCell(item) {
         const idx = this.cell?.order || 0;
