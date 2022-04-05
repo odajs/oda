bc774cbf6795bf1c614e10cb96f03e1db102bf57
ilysaz <ilysaz@gmail.com>
2022-04-05 14:30:04 +0300

    AutoCommit: 66839

--- learn/guide/my-first-component/oda-dino-game.js
+++ learn/guide/my-first-component/oda-dino-game.js
@@ -1,12 +1,45 @@
 function createCloud(){
     const gameSpace = document.getElementById('game-space');
-    gameSpace.append('oda-cloud');
-    newCloud = gameSpace.lastChild;
-    newCloud.style.top = randomInteger(settings.cloud.distance.min,
-        settings.cloud.distance.max) + 'px';
+    gameSpace.append(document.createElement('oda-cloud'));
+    const newCloud = gameSpace.lastChild;
+    const min = 10;
+    const max = 100;
+    newCloud.style.top = Math.floor(min + Math.random() * (max + 1 - min)) + 'px';
     newCloud.getAnimations().forEach((anim, i, arr) => {
         anim.onfinish = () => {
             newCloud.remove();
         };
     });
 }
+
+
+function createCactus(){
+    const gameSpace = document.getElementById('game-space');
+    gameSpace.append(document.createElement('oda-cactus'));
+    const newCactus = gameSpace.lastChild;
+    newCactus.getAnimations().forEach((anim, i, arr) => {
+        anim.onfinish = () => {
+            newCactus.remove();
+        };
+    });
+}
+
+function createPterodactyl(){
+    const gameSpace = document.getElementById('game-space');
+    gameSpace.append(document.createElement('oda-pterodactyl'));
+    const newPterodactyl = gameSpace.lastChild;
+    const min = 10;
+    const max = 100;
+    newPterodactyl.style.top = Math.floor(min + Math.random() * (max + 1 - min)) + 'px';
+    newPterodactyl.getAnimations().forEach((anim, i, arr) => {
+        anim.onfinish = () => {
+            newPterodactyl.remove();
+        };
+    });
+}
+
+createPterodactyl();
+
+createCloud();
+
+createCactus();
