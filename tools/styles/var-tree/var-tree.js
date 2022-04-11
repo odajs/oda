a37d4928f9ed95b19fa4e0cd52db2c8d960e9d6c
ilysaz <ilysaz@gmail.com>
2022-04-11 17:30:05 +0300

    AutoCommit: 67000

---
+++ tools/styles/var-tree/var-tree.js
@@ -0,0 +1,40 @@
+ODA({is:'oda-tools-styles-var-tree', imports: '@tools/property-grid', extends: 'oda-property-grid',
+    ready(){
+        const  result = Array.from(document.styleSheets).map(e=>{
+            const rules = Array.from(e.rules).filter(r=>r.selectorText === ':root')
+            return rules.map(s=>{
+                return Array.from(s.style).map(n=>{
+                        const v = s.styleMap.get(n);
+                        const vv = v.toLocaleString().trim();
+                        if (vv.startsWith('{')) return undefined;
+                        return [n, vv]
+                    }
+                ).filter(Boolean);
+            })
+        }).flat()
+        console.log(result)
+        const data = {}
+        const props = result.reduce((res, e, idx)=>{
+            e.forEach(obj =>{
+                res[obj[0]] = {
+                    default: obj[1],
+                    category: idx+1,
+                }
+                Object.defineProperty(data, obj[0], {
+                    configurable: true,
+                    set(n){
+                        ODA.updateStyle({[obj[0]]: n});
+                    },
+                    get(){
+                        return obj[1];
+                    }
+                })
+            })
+            return res;
+
+        }, {});
+        data.props = props;
+        this.inspectedObject = data;
+    },
+
+})
