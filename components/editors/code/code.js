5dfe015084842cf5250cd44e2b885719d4cc0ecb
ilysaz <ilysaz@gmail.com>
2022-04-20 12:00:05 +0300

    AutoCommit: 67195

--- components/editors/code/code.js
+++ components/editors/code/code.js
@@ -1,24 +1,52 @@
-ODA({is: 'oda-code',
+ODA({is: 'oda-code', imports: '@oda/splitter2, @oda/ace-editor, ../monaco/monaco.js',
     template:`  
         <style>
+            ::-webkit-scrollbar { width: 4px; height: 4px; }
+            ::-webkit-scrollbar-track { -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3); }
+            ::-webkit-scrollbar-thumb { border-radius: 10px; background: var(--body-background); -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.5); }
             :host{
                 @apply --horizontal;
+                @apply --flex;
+                min-height: 20px;
             }
         </style>
-        <div ~style="{backgroundColor: background}">
-            Code EDITOR
+        <div style="display: flex; flex: 1; overflow: hidden;" ~style="{height: editMode ? '80vh' : '100%' }">
+            <div ~if="preview==='none' || showCode || editMode" style="overflow: hidden; position: relative" ~style="{width: preview!=='none' ? '50%' : '100%'}">
+                <oda-ace-editor ~if="type==='ace'" :src="src" @change="onchange" class="flex" highlight-active-line="false" show-print-margin="false" theme="cobalt" mode="html" min-lines=1></oda-ace-editor></oda-ace-editor>
+                <oda-monaco ~if="type==='monaco'" :src="src" @change="onchange" class="flex" theme="vs-dark" mode="html"></oda-monaco>
+            </div>
+            <oda-splitter2 ~if="editMode && preview!=='none'" size="3px"></oda-splitter2>
+            <div ~if="preview!=='none'" class="flex" style="overflow: auto; flex: 1">
+                <div ~html="source"></div>
+            </div>
         </div>
-        
     `,
     props:{
-        editor:{
-            default: 'Первый',
-            list: ['Первый', 'Второй'],
+        type:{
+            default: 'ace',
+            list: ['ace', 'monaco'],
+            save: true,
+            set(n) {
+                this.src = this.source;
+            }
+        },
+        preview: {
+            default: 'preview',
+            list: ['none', 'preview'],
             save: true
         },
-        background: {
-            default: '',
+        showCode: {
+            default: false,
             save: true
         }
+    },
+    src: '',
+    set source(v) {
+        if (v !== undefined && !this.src)
+            this.src = v;
+    },
+    editMode: false,
+    onchange(e) {
+        this.source = e.detail.value;
     }
 })
--- components/editors/markdown/markdown.js
+++ components/editors/markdown/markdown.js
@@ -1,4 +1,4 @@
-ODA({is: 'oda-markdown', imports: '../simplemde/simplemde.js, @oda/splitter2, @oda/md-viewer',
+ODA({is: 'oda-markdown', imports: '../simplemde/simplemde.js, @oda/splitter2, @oda/md-viewer, @oda/ace-editor',
     template:`  
         <style>
             ::-webkit-scrollbar { width: 4px; height: 4px; }
@@ -16,8 +16,8 @@ ODA({is: 'oda-markdown', imports: '../simplemde/simplemde.js, @oda/splitter2, @o
                 <oda-ace-editor ~if="type==='ace'" :src="src" @change="onchange" class="flex" highlight-active-line="false" show-print-margin="false" theme="solarized_light" mode="markdown" min-lines=1></oda-ace-editor></oda-ace-editor>
             </div>
             <oda-splitter2 ~if="editMode" size="3px"></oda-splitter2>
-            <div class="flex" style="overflow: auto; flex: 1">
-                <oda-md-viewer class="flex" :srcmd="source" :edit-mode="editMode && !readOnly" padding="0"></oda-md-viewer>
+            <div style="overflow-x: hidden; flex: 1">
+                <oda-md-viewer class="flex" :srcmd="source" :edit-mode="editMode && !readOnly" padding="0 2px"></oda-md-viewer>
             </div>
         </div>
     `,
