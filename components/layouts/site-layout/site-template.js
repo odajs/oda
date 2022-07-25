4c4a634d24884dfc02667871acbe6392527ecafe
ilysaz <ilysaz@gmail.com>
2022-07-25 15:30:05 +0300

    AutoCommit: 69423

---
+++ components/layouts/site-layout/site-template.js
@@ -0,0 +1,22 @@
+ODA({is: 'oda-site-template', extends: 'oda-css', imports: '@oda/icon, ',
+    template: /*html*/ `
+    <div  id='site-all' :class='_class()'>
+        <slot name='region-tuoolbar' id='region-tuoolbar'></slot>
+        <slot name='region-mmenu' id='region-mmenu'></slot>
+        <slot name='region-ltuoolbar' id='region-ltuoolbar'></slot>
+        <slot name='region-rtuoolbar' id='region-rtuoolbar'></slot>
+        <div id='site-main'> 
+            <slot name='region-header' id='region-header'></slot>
+            <slot name='region-left' id='region-left'></slot>
+            <slot name='region-abovecontrnt' id='region-abovecontrnt'></slot>
+            <slot name='region-contrnt' id='region-contrnt'></slot>
+            <slot name='region-undercontrnt' id='region-undercontrnt'></slot>
+            <slot name='region-right' id='region-right'></slot>
+            <slot name='region-footer' id='region-footer'></slot>
+        </div>
+        <slot name='region-outbottom' id='region-outbottom'></slot>
+    </div>
+
+    `,
+
+})
