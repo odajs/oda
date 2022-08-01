edea761157cc1915b75ab7bab9ea382b44c65682
ilysaz <ilysaz@gmail.com>
2022-08-01 14:30:05 +0300

    AutoCommit: 69463

--- components/layouts/site-layout/site-test.js
+++ components/layouts/site-layout/site-test.js
@@ -1,46 +1,34 @@
-// 'ГЛАВНАЯ УЧЕБНЫЙ ЦЕНТР ДОКУМЕНТАЦИЯ БЛОГ ПАРТНЕРАМ ЦЕНЫ РЕЕСТР ЛИЦЕНЗИЙ ОНАС'
 
+// Пока сделаю лендинг
 import './site-template.js';
-ODA({ is: 'oda-site-test', extends: 'oda-site-template', template: `
-<oda-site-footer></oda-site-footer>
-`,
+ODA({ is: 'oda-site-test', extends: 'oda-css', template: /*html*/ `
+    <oda-site-header :css></oda-site-header>
+    <oda-site-footer :css></oda-site-footer>
+    `,
+    props:{
+        css:'./default.css',
+        sections:[]
+    },
 });
 
 
-ODA({ is: 'oda-site-footer', template: `
-    <style>
-        :host {
-            @apply --flex;
-            min-height: 400px;
-            @apply --header;
-        }    
-    </style>
-    <span ~for="menu">Menu {{item}}</span>
-    `,
-    props: {
-        menu: [
-            1, 2, 3, 4
-        ]
-    }
+ODA({ is: 'oda-site-header', extends: 'oda-css', template: /*html*/ `
+    <div id='flogo'><img src='svg/logo_platform-min.svg'/></div>
+    <slot name='main-menu'></slot>
+    `
 });
 
-ODA({ is: 'oda-site-content', template: `
-    <style>
-        :host {
-            @apply --vertical;
-            @apply --flex;
-            overflow: auto;
-        }   
-        
-        span {
-            min-height: 100vh;
-        } 
-    </style>
-    <span ~for="pages">Page {{item}}</span>
+
+ODA({ is: 'oda-site-footer', extends: 'oda-css', template: /*html*/ `
+    <div class='fcontent' ~html='content'></div>
+    <div id='soc-menu'>
+        <a ~for='socLinck' :href='item?.linck' :title='item?.title'><img :src='item?.img' /></a>
+    </div>
     `,
     props: {
-        pages: [
-            1, 2, 3, 4
-        ]
+        socLinck:[{img:'test-soc/1.png', title:'weChat', linck:'/'},{img:'test-soc/3.png', title:'telegram', linck:'/'},
+                    {img:'test-soc/2.png', title:'vk', linck:'/'},{img:'test-soc/4.png', title:'skype', linck:'/'}],
+        content: /*html*/  `Copyright © 2015 BusinessInterSoft, <br/> LLC. ODANT® - is a registered trademark of BusinessInterSoft, LLC.`
+
     }
 });
