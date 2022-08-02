4cb504bc5a70c375b2deb47486437a548914ce99
ilysaz <ilysaz@gmail.com>
2022-08-02 14:30:06 +0300

    AutoCommit: 69474

---
+++ components/layouts/site-layout/content.js
@@ -0,0 +1,86 @@
+
+export let sectionsContent = [
+    {body: `<img src='ddd.jpg'>`},
+    {inmenu: 'АВТОМАТИЗАЦИЯ',
+    header: 'Подход ODANT',
+    body: `
+    <div class="elementor-widget-wrap">
+    <div class="elementor-element elementor-element-c9e3d66 elementor-widget elementor-widget-spacer" data-id="c9e3d66" data-element_type="widget" data-widget_type="spacer.default">
+<div class="elementor-widget-container">
+<div class="elementor-spacer">
+<div class="elementor-spacer-inner"></div>
+</div>
+</div>
+</div>
+<div class="elementor-element elementor-element-0c0fd00 elementor-widget elementor-widget-heading" data-id="0c0fd00" data-element_type="widget" data-widget_type="heading.default">
+<div class="elementor-widget-container">
+<h3 class="elementor-heading-title elementor-size-default">Подход ODANT</h3>		</div>
+</div>
+<div class="elementor-element elementor-element-88514b9 elementor-widget elementor-widget-spacer" data-id="88514b9" data-element_type="widget" data-widget_type="spacer.default">
+<div class="elementor-widget-container">
+<div class="elementor-spacer">
+<div class="elementor-spacer-inner"></div>
+</div>
+</div>
+</div>
+<div class="elementor-element elementor-element-a28d7b3 elementor-widget elementor-widget-text-editor" data-id="a28d7b3" data-element_type="widget" data-widget_type="text-editor.default">
+<div class="elementor-widget-container">
+<div class="elementor-text-editor elementor-clearfix"><p>При разработке на платформе ODANT бизнес-инженер непосредственно реализует модель данных и бизнес-логику с помощью визуальных инструментов. Программисты пишут код ранее не реализованных методов и представлений.&nbsp;</p></div>
+</div>
+</div>
+<div class="elementor-element elementor-element-ffe9e61 elementor-widget elementor-widget-image" data-id="ffe9e61" data-element_type="widget" data-widget_type="image.default">
+<div class="elementor-widget-container">
+<div class="elementor-image">
+                    <img width="768" height="366" src="https://legacy.odant.ru/wp-content/uploads/2018/06/odant1-768x366.png" class="attachment-medium_large size-medium_large" alt="" loading="lazy" srcset="https://legacy.odant.ru/wp-content/uploads/2018/06/odant1-768x366.png 768w, https://legacy.odant.ru/wp-content/uploads/2018/06/odant1-300x143.png 300w, https://legacy.odant.ru/wp-content/uploads/2018/06/odant1.png 1000w" sizes="(max-width: 768px) 100vw, 768px">											</div>
+</div>
+</div>
+<div class="elementor-element elementor-element-9203a06 elementor-widget elementor-widget-text-editor" data-id="9203a06" data-element_type="widget" data-widget_type="text-editor.default">
+<div class="elementor-widget-container">
+<div class="elementor-text-editor elementor-clearfix"><p>Компонентный подход предполагает, что каждая элементарная сущность реализуется независимо от других минимальной командой разработчиков и соответствует концепции MVC.</p></div>
+</div>
+</div>
+<div class="elementor-element elementor-element-d8cb4e3 elementor-widget elementor-widget-image" data-id="d8cb4e3" data-element_type="widget" data-widget_type="image.default">
+<div class="elementor-widget-container">
+<div class="elementor-image">
+                    <img width="768" height="366" src="https://legacy.odant.ru/wp-content/uploads/2018/06/odant2-768x366.png" class="attachment-medium_large size-medium_large" alt="" loading="lazy" srcset="https://legacy.odant.ru/wp-content/uploads/2018/06/odant2-768x366.png 768w, https://legacy.odant.ru/wp-content/uploads/2018/06/odant2-300x143.png 300w, https://legacy.odant.ru/wp-content/uploads/2018/06/odant2.png 1000w" sizes="(max-width: 768px) 100vw, 768px">											</div>
+</div>
+</div>
+<div class="elementor-element elementor-element-9958154 elementor-widget elementor-widget-spacer" data-id="9958154" data-element_type="widget" data-widget_type="spacer.default">
+<div class="elementor-widget-container">
+<div class="elementor-spacer">
+<div class="elementor-spacer-inner"></div>
+</div>
+</div>
+</div>
+    </div>`
+    },
+    {inmenu: 'СВИДЕТЕЛЬСТВА',
+    header: 'Свидетельства Роспатент',
+    body: `<div class="grid"><div class="cell"><figure><img src="https://odant.org/img/certificate/tovarnyy-znak.jpg"><figcaption> <div class="cert-header">Товарный знак</div>
+    <div>Свидетельство № 491495</div></figcaption></figure></div><div class="cell"><figure><img src="https://odant.org/img/certificate/webs-server-odant.jpg"><figcaption> 
+    <div class="cert-header">Web-сервер ODANT</div><div>Свидетельство №2012615884</div></figcaption></figure></div><div class="cell"><figure>
+    <img src="https://odant.org/img/certificate/parametry.jpg"><figcaption> <div class="cert-header">Параметры</div><div>Свидетельство №2012615884</div></figcaption>
+    </figure></div><div class="cell"><figure><img src="https://odant.org/img/certificate/parser.jpg"><figcaption> <div class="cert-header">Парсер ODANT	</div>
+    <div>Свидетельство №2012616275</div></figcaption></figure></div><div class="cell"><figure><img src="https://odant.org/img/certificate/client-odant.jpg"><figcaption>
+     <div class="cert-header">Клиент ODANT	</div><div>Свидетельство №2012615885</div></figcaption></figure></div><div class="cell"><figure>
+     <img src="https://odant.org/img/certificate/marshrutizator.jpg"><figcaption> <div class="cert-header">Маршрутизатор ODANT</div><div>Свидетельство №2012616185</div>
+     </figcaption></figure></div><div class="cell"><figure><img src="https://odant.org/img/certificate/programma-server-odant.jpg"><figcaption> 
+     <div class="cert-header">Программа Сервер ODANT</div><div>Свидетельство №2012616186</div></figcaption></figure></div><div class="cell">
+     <figure><img src="https://odant.org/img/certificate/grafiki-i-diagramy.jpg"><figcaption> <div class="cert-header">Обработчик графиков и диаграмм</div>
+     <div>Свидетельство №2013660522</div></figcaption></figure></div><div class="cell"><figure><img src="https://odant.org/img/certificate/obrabotchiki-bizness-pokazateley.jpg">
+     <figcaption> <div class="cert-header">Обработчик бизнес показателей</div><div>Свидетельство №2013660520</div></figcaption></figure></div><div class="cell">
+     <figure><img src="https://odant.org/img/certificate/buhg.jpg"><figcaption> <div class="cert-header">Бухгалтерская отчетность</div><div>Свидетельство №2013660521</div>
+     </figcaption></figure></div><div class="cell"><figure><img src="https://odant.org/img/certificate/fiz-ozdor.jpg"><figcaption> 
+     <div class="cert-header">Автоматизированная система управления физкультурно-оздоровительными комплексами</div><div>Свидетельство №2013610914</div>
+     </figcaption></figure></div><div class="cell"><figure><img src="https://odant.org/img/certificate/zakup.jpg"><figcaption> 
+     <div class="cert-header">Автоматизированная система управления и контроля над осуществлением государственных закупок</div>
+     <div>Свидетельство №2013614039</div></figcaption></figure></div><div class="cell"><figure><img src="https://odant.org/img/certificate/posesh.jpg"><figcaption> 
+     <div class="cert-header">Модуль планирования и учета посещений</div><div>Свидетельство № 2013614944</div></figcaption></figure></div><div class="cell">
+     <figure><img src="https://odant.org/img/certificate/fastreport.jpg"><figcaption> <div class="cert-header">Обработчик FastReport</div><div>Свидетельство №2013661095</div>
+     </figcaption></figure></div><div class="cell"><figure><img src="https://odant.org/img/certificate/sber.jpg"><figcaption> 
+     <div class="cert-header">Модуль для подключения эквайринговой системы "Сбербанк"</div><div>Свидетельство №2013617193</div></figcaption></figure></div>
+     <div class="cell"><figure><img src="https://odant.org/img/certificate/kass.jpg"><figcaption> <div class="cert-header">Модуль управления кассовым аппаратом</div>
+     <div>Свидетельство №2013618771</div></figcaption></figure></div></div>
+    `
+    }
+]
--- components/layouts/site-layout/site-test.js
+++ components/layouts/site-layout/site-test.js
@@ -1,33 +1,43 @@
 
+import {sectionsContent} from './content.js';
 // Пока сделаю лендинг
 import './site-template.js';
-ODA({ is: 'oda-site-test', extends: 'oda-css', template: /*html*/ `
+ODA({
+    is: 'oda-site-test', extends: 'oda-css', template: /*html*/ `
     <oda-site-header :css></oda-site-header>
+    <div ~for='sections' class='content'>
+        <div ~if='item?.inmenu' class='menu' slot='mainmenu'><a :href='"#sec"+index'>{{item?.inmenu}}</a></div>
+        <a ~if='item?.inmenu' :name='"#sec"+index'></a>
+        <h2 ~if='item?.header' class='secname'>{{item?.header}}</h2>
+        <div ~if='item?.body' class='secbody' ~html='item?.body'></div>
+    </div>
     <oda-site-footer :css></oda-site-footer>
     `,
-    props:{
-        css:'./default.css',
-        sections:[]
+    props: {
+        css: './default.css',
+        sections: sectionsContent
     },
 });
 
 
-ODA({ is: 'oda-site-header', extends: 'oda-css', template: /*html*/ `
+ODA({
+    is: 'oda-site-header', extends: 'oda-css', template: /*html*/ `
     <div id='flogo'><img src='svg/logo_platform-min.svg'/></div>
-    <slot name='main-menu'></slot>
+    <slot name='mainmenu'></slot>
     `
 });
 
 
-ODA({ is: 'oda-site-footer', extends: 'oda-css', template: /*html*/ `
+ODA({
+    is: 'oda-site-footer', extends: 'oda-css', template: /*html*/ `
     <div class='fcontent' ~html='content'></div>
     <div id='soc-menu'>
         <a ~for='socLinck' :href='item?.linck' :title='item?.title'><img :src='item?.img' /></a>
     </div>
     `,
     props: {
-        socLinck:[{img:'test-soc/1.png', title:'weChat', linck:'/'},{img:'test-soc/3.png', title:'telegram', linck:'/'},
-                    {img:'test-soc/2.png', title:'vk', linck:'/'},{img:'test-soc/4.png', title:'skype', linck:'/'}],
+        socLinck: [{ img: 'test-soc/1.png', title: 'weChat', linck: '/' }, { img: 'test-soc/3.png', title: 'telegram', linck: '/' },
+        { img: 'test-soc/2.png', title: 'vk', linck: '/' }, { img: 'test-soc/4.png', title: 'skype', linck: '/' }],
         content: /*html*/  `Copyright © 2015 BusinessInterSoft, <br/> LLC. ODANT® - is a registered trademark of BusinessInterSoft, LLC.`
 
     }
