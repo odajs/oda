878b087c00c204ebec302983991dce0c51adf7c3
ilysaz <ilysaz@gmail.com>
2022-08-05 15:00:05 +0300

    AutoCommit: 69536

--- components/layouts/site-layout/site-test.js
+++ components/layouts/site-layout/site-test.js
@@ -5,12 +5,17 @@ import './site-template.js';
 ODA({
     is: 'oda-site-test', /*extends: 'oda-css',*/ template: /*html*/ `
     <style>
-        :host{width:100%; height: 100%; overflow: scroll;}
+        :host{width:100%; height: 100%; overflow: scroll;scroll-behavior: smooth;}
+        .fix {position:absolute; top:0%;}
+        #header {width:100%; background:whitesmoke; border-bottom:2px solid #007fc6;}
+        oda-site-header {padding:2% 10%;}
+        .fix oda-site-header {padding:5px 10%;}
     </style>
-    <oda-site-header ></oda-site-header>
-    <div ~for='sections' class='content'>
-        <div ~if='item?.inmenu' class='menu' slot='mainmenu'><a :href='"#sec"+index'>{{item?.inmenu}}</a></div>
-        <a ~if='item?.inmenu' :name='"#sec"+index'></a>
+    <div id='header' :class='fixmenu'>
+        <oda-site-header :fixmenu ></oda-site-header>
+    </div>
+    <div ~for='sections' class='content' ~ref='"sec"+index'>
+        <div ~if='item?.inmenu' class='menu' slot='mainmenu'><a @tap='_go(index)' :href='"#sec"+index'>{{item?.inmenu}}</a></div>
         <h2 ~if='item?.header' class='secname'>{{item?.header}}</h2>
         <div ~if='item?.body' class='secbody' ~html='item?.body'></div>
     </div>
@@ -19,18 +24,23 @@ ODA({
     props: {
         // css: './default.css',
         sections: sectionsContent,
-        // w:{ get() {return this.offsetWidth} }
+        fixmenu: 'nofix',
     },
     listeners: {
         'resize': '_resize',
         'scroll': '_scrol'
-
     },
     _resize() {
         console.log(this.offsetWidth, this.offsetHeight) 
     },
 
-    _scrol(){ console.log(this.scrollTop)}
+    _scrol(){
+        this.fixmenu = (this.scrollTop > 10) ? 'fix' : 'nofix' 
+        // console.log(this.scrollTop)
+    },
+    _go(i) {console.log(this.$refs['sec'+i][0])
+        this.scrollTop = this.$refs['sec'+i][0].offsetTop
+    }
 
 });
 
@@ -38,12 +48,12 @@ ODA({
 ODA({
     is: 'oda-site-header',/* extends: 'oda-css', */template: /*html*/ `
     <style>
-        :host {display:flex;padding:2% 10%;align-items: center; justify-content: space-between;}
+        :host {display:flex;align-items: center; justify-content: space-between; transition: all 0.5s linear;}
         #flogo {width: 20%;}
-
+        #flogo.fix img {height:40px}
         .menu {padding:0 0 0 10px;}
     </style>
-    <div id='flogo'><img src='svg/logo_platform-min.svg'/></div>
+    <div id='flogo' :class='fixmenu'><img src='svg/logo_platform-min.svg'/></div>
 
     <slot name='mainmenu' class='mainmenu'></slot>
     `
