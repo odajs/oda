88ba5340e60144b16ecbd6142564d5a886e9583b
ilysaz <ilysaz@gmail.com>
2022-10-10 14:30:04 +0300

    AutoCommit: 70969

--- components/inputs/date-time/date-time.js
+++ components/inputs/date-time/date-time.js
@@ -2,20 +2,139 @@ ODA({
     is: 'oda-date-time-input',
     template: `
         <style>
+             :host{
+                @apply --vertical;
+                @apply --border;
+            }
             input{
                 outline: none;
-                text-align: right;
                 text-overflow: ellipsis;
-                width: auto;
+                border: none;
             }
         </style>
         <input type="text" :value="valueText" @keydown="onKeyDown"  @input="onValueChanged">
     `,
     get valueText(){
-        var options = {timeZone: 'UTC', timeZoneName: 'long', weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
+        var options = {}
+        if (this.hour12){
+            options.hour12 = this.hour12;
+        }
+        if (this.timeZone !== 'none'){
+            options.timeZone = this.timeZone;
+        }
+        switch (this.weekday){
+            case 'narrow':
+            case 'short':
+            case 'long':{
+                options.weekday = this.weekday;
+            } break;
+        }
+        switch (this.era){
+            case 'narrow':
+            case 'short':
+            case 'long':{
+                options.era = this.era;
+            } break;
+        }
+        switch (this.year){
+            case 'numeric':
+            case '2-digit':{
+                options.year = this.year;
+            } break;
+        }
+        switch (this.month){
+            case  "numeric":
+            case "2-digit":
+            case "narrow":
+            case "short":
+            case "long":{
+                options.month = this.month;
+            } break;
+        }
+        switch (this.day){
+            case 'numeric':
+            case '2-digit':{
+                options.day = this.day;
+            } break;
+        }
+        switch (this.hour){
+            case 'numeric':
+            case '2-digit':{
+                options.hour = this.hour;
+            } break;
+        }
+        switch (this.minute){
+            case 'numeric':
+            case '2-digit':{
+                options.minute = this.minute;
+            } break;
+        }
+        switch (this.second){
+            case 'numeric':
+            case '2-digit':{
+                options.second = this.second;
+            } break;
+        }
+        switch (this.timeZoneName){
+            case 'short':
+            case 'long':{
+                options.timeZoneName = this.timeZoneName;
+            } break;
+        }
         return this.value?.toLocaleString(this.locale, options) || ''
     },
     props:{
+        hour12: false,
+        timeZone: {
+            default: 'none',
+            list: ['none', "UTC"],
+            category: 'format',
+        },
+        timeZoneName:{
+            default: 'none',
+            list: ['none', "short", "long"],
+            category: 'format',
+        },
+        weekday:{
+            default: 'none',
+            list: ['none', "narrow", "short" , "long"],
+            category: 'format',
+        },
+        era:{
+            default: 'none',
+            list: ['none', "narrow", "short" , "long"],
+            category: 'format',
+        },
+        year:{
+            default: 'none',
+            list: ['none', "numeric", "2-digit"],
+            category: 'format',
+        },
+        month: {
+            default: 'none',
+            list: ['none', "numeric", "2-digit", "narrow", "short", "long"],
+            category: 'format',
+        },
+        day: {
+            default: 'none',
+            list: ['none', "numeric", "2-digit"],
+            category: 'format',
+        },
+        hour: {
+            default: 'none',
+            list: ['none', "numeric", "2-digit"],
+            category: 'format',
+        },
+        minute: {
+            default: 'none',
+            list: ['none', "numeric", "2-digit"],
+            category: 'format',
+        },
+        second: {
+            default: 'none',
+            list: ['none', "numeric", "2-digit"],
+            category: 'format',
+        },
         locale: {
             default: 'ru-RU',
             get list(){
@@ -32,6 +151,7 @@ ODA({
         },
         value: {
             type: Date,
+            readOnly: true,
             set(n){
                 if (n === 0){
                     this.render();
