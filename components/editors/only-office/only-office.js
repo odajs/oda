84b59f12e024f7f42cbcabc210fabcb9f1419280
ilysaz <ilysaz@gmail.com>
2023-12-06 18:30:20 +0300

    AutoCommit: 80480

--- components/editors/only-office/only-office.js
+++ components/editors/only-office/only-office.js
@@ -8,9 +8,17 @@ ODA({is: 'oda-only-office',
                 @apply --flex;
             }
         </style>
-        <iframe class="flex" :src="frameUrl"></iframe>      
+        <iframe class="flex"></iframe>      
     `,
     get frameUrl(){
-        return path;
+        return path + '#' + this.url;
+    },
+    get url(){
+        return "https://odajs.org/components/editors/onlyoffice-editor/demo/demo.docx";
+    },
+    attached(){
+        this.async(()=>{
+            this.$('iframe').src = this.frameUrl;
+        }, 100)
     }
 })
