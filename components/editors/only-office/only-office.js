c34e81b1611cd99476fa4fcb6adbce0c9adde034
ilysaz <ilysaz@gmail.com>
2023-12-07 10:30:27 +0300

    AutoCommit: 80488

--- components/editors/only-office/only-office.js
+++ components/editors/only-office/only-office.js
@@ -1,6 +1,6 @@
-const path = import.meta.url.split('only-office.js')[0]+'frame.html'
-ODA({is: 'oda-only-office',
-    template:`
+const path = import.meta.url.split('only-office.js')[0] + 'frame.html';
+ODA({ is: 'oda-only-office',
+    template: `
         <style>
             :host{
                 @apply --vertical;
@@ -10,15 +10,27 @@ ODA({is: 'oda-only-office',
         </style>
         <iframe class="flex"></iframe>      
     `,
-    get frameUrl(){
-        return path + '#' + this.url;
-    },
-    get url(){
+    get url() {
         return "https://odajs.org/components/editors/onlyoffice-editor/demo/demo.docx";
     },
-    attached(){
-        this.async(()=>{
-            this.$('iframe').src = this.frameUrl;
-        }, 100)
+    attached() {
+        this.iframe = this.$('iframe');
+        this.iframe.addEventListener('load', () => {
+            this.docEditor = new this.iframe.contentWindow.DocsAPI.DocEditor("placeholder",
+                {
+                    "document": {
+                        "fileType": "docx",
+                        "url": this.url,
+                        "title": this.url.split('/').pop(),
+                    },
+                    "documentType": "word",
+                    // "type": "embedded",
+                    "editorConfig": {
+                        // "mode": 'view',
+                    }
+                }
+            )
+        })
+        this.iframe.src = path;
     }
 })
