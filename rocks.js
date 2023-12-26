f431299b35a6c76f97e6e292004727e6dc117fa9
ilysaz <ilysaz@gmail.com>
2023-12-26 11:00:27 +0300

    AutoCommit: 80705

--- rocks.js
+++ rocks.js
@@ -836,6 +836,19 @@ in the <${host.localName}>`;
                 }
             })
         }
+        if (!String.prototype.hashCode) {
+            String.prototype.hashCode = function () {
+                let hash = 0,
+                    i, chr;
+                if (this.length === 0) return hash;
+                for (i = 0; i < this.length; i++) {
+                    chr = this.charCodeAt(i);
+                    hash = ((hash << 5) - hash) + chr;
+                    hash |= 0;
+                }
+                return hash;
+            }
+        }
     }
 }
 export default globalThis.ROCKS;
