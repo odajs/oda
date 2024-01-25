384ed650b9d93d29236c6dc0727a384d3fd5fab8
ilysaz <ilysaz@gmail.com>
2024-01-25 10:30:21 +0300

    AutoCommit: 80953

--- apps/neuronet/attoGPT/zipper.js
+++ apps/neuronet/attoGPT/zipper.js
@@ -1,5 +1,6 @@
-// Сам класс new Zipper(<fun>,<argsDem>) создает класс наследуемый от функции и который можно использовать как функцию
-// где <fun> -- функция запаковки, <argsDem> -- список размерности аргументов принимаемых <fun>
+// Сам класс new Zipper(<fun>,<argsDem> [, <strict>]) создает класс наследуемый от функции и который можно использовать как функцию
+// где <fun> -- функция запаковки, <argsDem> -- список размерности аргументов принимаемых <fun>, 
+//     <strict> -- размерность аргументов строго больше размерности целевой ф-и, по умолчанию false
 class Zipper extends Function{
     constructor(fun,argsDem,strict=false) {
         super()
@@ -12,13 +13,18 @@ class Zipper extends Function{
     }
     __call__() {
         let args = [...arguments]
-        // console.log('args=',args)
         let err0 = (n1,n2) => {console.error(`Zipper: Ошибка, количество аргументов функции ${n1}, но подали ${n2}.`)}
         if (this.argsDem.length != args.length) {err0(this.argsDem.length,args.length); return} // проверили кол-во аргументов
-        // ! тут должна быть ко расширение при не строгой размерности, но позже (и без этого почти работает)
+
         let getShape = (ts,akk=[]) => (Array.isArray(ts))? getShape(ts[0],akk.concat([ts.length])):akk
         let argShapes = args.map(arg => getShape(arg)) // посчитали Shape для всех аргументов
-        // console.log('argShapes=',argShapes)
+        let err1 = (i, dIn ,dFun) => {console.error(`Zipper: Ошибка ${i} аргумента, размерность: ${dIn}. Размерность ф-и: ${dFun}. Установите strict=false.`)}
+        argShapes.forEach((s,i)=> { // расширим, 
+            let dem =this.argsDem[i] - s.length 
+            return (dem>0)? this.strict? err1(i,s.length,this.argsDem[i]): args[i] = [...new Array(dem)].reduce(akk=>[akk],args[i]): {}
+        })
+        if (!this.strict) argShapes = args.map(arg => getShape(arg)) // поправим размерность
+
         let err2 = (nom, l ,l1,l2) => {console.error(`Zipper: Ошибка аргумента ${nom} на уровне ${l}. Размерность ${l1}, но есть ${l2}.`)}
         let testShapes = (shape,arg,nom,level=0) => {
             if (shape[0]===undefined) return true
@@ -31,21 +37,16 @@ class Zipper extends Function{
         let maxDepth =  Math.max(...maskShapes.map(s => s.length))
         maskShapes = maskShapes.map(s => new Array(maxDepth-s.length).fill(0).concat(s))
 
-        // console.log('maskShapes=',maskShapes)
         let rezShape = maskShapes[0].map((_,i) => {            
             let maxS = maskShapes.map(arg => arg[i]).filter(d => d>1)
-            // console.log('maxS=',maxS)
             return  (maxS.length==0)? 1 : (maxS.length==1)? maxS[0]: -1
         } )
-        // console.log('rezShape=',rezShape)
+        
         let err3 = () => console.error(`Zipper: Неизвестно как такое Зиппить`)
         if (rezShape.includes(-1)) { err3(); return }
 
         let makeMagic = (funArgs, shape, l=0) => {
-            if (shape.length==0) {
-                // console.log('funArgs=',funArgs)
-                return this.fun(...funArgs)
-            }
+            if (shape.length==0) return this.fun(...funArgs)
             let newArgs = []
             for (let j=0;j<shape[0];j++) newArgs[j] = args.map((a,i)=> 
                 (maskShapes[i][l]==0)? funArgs[i] : (maskShapes[i][l]==1)? funArgs[i][0] : funArgs[i][j]  )
@@ -59,12 +60,13 @@ class Zipper extends Function{
 // testTen(<shape>,<filling>) -- ф-я для удобства создания тестовых примеров, где <shape> -- размерность, <filling> -- ф-я заполнения
 function testTen(shape,filling) {
     if (shape[0] == undefined) return filling()
-    else return (new Array(shape[0])).fill(0).map(() => testTen(shape.slice(1),filling) )
+    else return [...new Array(shape[0])].map(() => testTen(shape.slice(1),filling) )
 }
 // Набор тестовых аргументов
 let x = testTen([2,3,1],()=>2)
 let y = testTen([2],()=>(Math.random()*10).toFixed(0)*1) // заполнено целыми от [0-9]
 let bad = [[[1,1],[2,2]],[[3,3],[4,4],[5,5]]] // пример поломанного аргумента
+// console.log(x,y)
 
 // Тестовые ф-и
 // ф-я одного аргумента нулевой размерности (число), возводит в квадрат
@@ -77,7 +79,7 @@ let concatZ =  new Zipper((a,b)=>a.concat(b),[1,1])
 
 
 let concat3 =  new Zipper((a,b,c)=>a.concat(b).concat(c), [1,1,1] )
-// console.log('concat3 = ',concat3([9],x,y)) //
+console.log('concat3 = ',concat3(9,x,y)) //
 
 // размерность может быть разной 
 let fun3 =  new Zipper((a,b,c)=> [a,b[0],c[0][0]], [0,1,2] )
