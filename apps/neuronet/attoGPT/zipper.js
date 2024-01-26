0a87f55478fbf002c875e8a06f16addbe06a6109
ilysaz <ilysaz@gmail.com>
2024-01-26 06:30:46 +0300

    AutoCommit: 80964

--- apps/neuronet/attoGPT/zipper.js
+++ apps/neuronet/attoGPT/zipper.js
@@ -3,16 +3,10 @@
 //     <strict> -- размерность аргументов строго больше размерности целевой ф-и, по умолчанию false
 
 export const ERROR = {
-    NUM_ARGUMENTS_NOT_MATCH: (num_args, ) => new Error(`Item "${pathname}" not found`),
-    // let err0 = (n1,n2) => {
-    //     throw new Error()
-    //     console.error(`Zipper: Ошибка, количество аргументов функции ${n1}, но подали ${n2}.`)
-    // }
-    ITEM_NOT_FOUND: (pathname) => new Error(`Item "${pathname}" not found`),
-    UNKNOWN_ITEM_METHOD: (item, method) => new Error(`Unknown method "${method}" for item "${JSON.stringify(item)}"`),
-    FILE_NOT_FOUND: (path) => new Error(`File "${path}" not found`),
-    UNAVAILABLE_ITEM_METHOD: (descr) => new Error(`Unavailable item method "${JSON.stringify(descr)}"`),
-    LOADED_MODULE_CORRUPTED: (pathname) => new Error(`Loaded module "${pathname}" is corrupted`),
+    NUM_ARGUMENTS: (args_fun, args_in) => new Error(`Zipper: У zip-функции ${args_fun} аргуметов, но подаем ${args_in}.`),
+    ARGUMENT_BAD: (nom,l,l1,l2) => new Error(`Zipper: Ошибка ${nom} аргумента  на уровне ${l}. Размерность ${l1}, но есть ${l2}.`),
+    SHAPE_LESS_FUN: (i, dIn ,dFun) => new Error(`Zipper: Ошибка ${i} аргумента, размерность: ${dIn}. Размерность ф-и: ${dFun}. Установите strict=false.`),
+    NO_SOLUTIONS: (level,arg,d1,d2) => new Error(`Zipper: Аргумент ${arg} на уровне ${level} размерности ${d1}, а требует ${d2}. Нет решения.`),
 }
 
 export default class Zipper extends Function{
@@ -27,54 +21,38 @@ export default class Zipper extends Function{
     }
     __call__() {
         let args = [...arguments]
-        let err0 = (n1,n2) => {
-            // throw new Error()
-            console.error(`Zipper: Ошибка, количество аргументов функции ${n1}, но подали ${n2}.`)
-        }
-        if (this.argsDem.length !== args.length) { // проверили кол-во аргументов
-            // ERROR.NUM_ARGUMENTS_NOT_MATCH(this.argsDem.length,args.length);
-            err0(this.argsDem.length,args.length); return
-        }
+        if (this.argsDem.length !== args.length) ERROR.NUM_ARGUMENTS (this.argsDem.length,args.length)
 
         let getShape = (ts,akk=[]) => (Array.isArray(ts))? getShape(ts[0],akk.concat([ts.length])):akk
         let argShapes = args.map(arg => getShape(arg)) // посчитали Shape для всех аргументов
-        let err1 = (i, dIn ,dFun) => {console.error(`Zipper: Ошибка ${i} аргумента, размерность: ${dIn}. Размерность ф-и: ${dFun}. Установите strict=false.`)}
-        argShapes.forEach((s,i)=> { // расширим, 
-            let dem =this.argsDem[i] - s.length 
-            return (dem>0)? this.strict? err1(i,s.length,this.argsDem[i]): args[i] = [...new Array(dem)].reduce(akk=>[akk],args[i]): {}
-        })
-        if (!this.strict) argShapes = args.map(arg => getShape(arg)) // поправим размерность
 
-        let err2 = (nom, l ,l1,l2) => {
-            console.error(`Zipper: Ошибка аргумента ${nom} на уровне ${l}. Размерность ${l1}, но есть ${l2}.`)
-        }
-        let testShapes = (shape,arg,nom,level=0) => {
-            if (shape[0] === undefined) return true
-            if (shape[0] !==  arg.length) {err2(nom,level,shape[0],arg.length); return false}
-            return arg.map(sA => testShapes(shape.slice(1),sA,nom,level++) ).every((a)=>a)            
-        }
-        if (argShapes.map((s,i) => testShapes(s,args[i],i) ).every((a)=>!a) ) return // проверили размерность входящих аргументов
-        
-        let maskShapes = argShapes.map((s,i) => s.slice(0,s.length-this.argsDem[i]) )
-        let maxDepth =  Math.max(...maskShapes.map(s => s.length))
-        maskShapes = maskShapes.map(s => new Array(maxDepth-s.length).fill(0).concat(s))
+        let testShapes = (shape,arg,nom,level=0) => 
+            !shape[0]? {}: (shape[0] !==  arg.length)? ERROR.ARGUMENT_BAD (nom,level,shape[0],arg.length)
+            : arg.forEach(sA => testShapes(shape.slice(1),sA,nom,level++) )
+        argShapes.forEach((s,i) => testShapes(s,args[i],i)) // проверили размерность входящих аргументов
 
-        let rezShape = maskShapes[0].map((_,i) => {            
-            let maxS = maskShapes.map(arg => arg[i]).filter(d => d>1)
-            return  (maxS.length === 0)? 1 : (maxS.length === 1)? maxS[0]: -1
+        let sadowShapes = argShapes.map((s,i) => s.slice(0,-this.argsDem[i]))
+        let rezShape = sadowShapes.reduce((akk,cur) => (akk.length>cur.length)?akk:cur, [])
+
+        argShapes.forEach((s,i) => {
+            let deltaR = ((a,b)=>(a>b)? a-b: this.strict? ERROR.SHAPE_LESS_FUN(i,b,a): 0)(this.argsDem[i], s.length)  
+            let deltaL = rezShape.length - sadowShapes[i].length
+            args[i] = [...new Array(deltaR+deltaL)].reduce(akk=>[akk],args[i])
+            argShapes[i] = getShape(args[i])
         })
-        
-        let err3 = () => console.error(`Zipper: Неизвестно как такое Зиппить`)
-        if (rezShape.includes(-1)) { err3(); return }
+        sadowShapes = argShapes.map((s,i) => s.slice(0,-this.argsDem[i]))
+
+        let col=(new Array(sadowShapes[0].length)).fill(1)
+        sadowShapes.forEach((s,i)=> s.forEach((dem,j)=> 
+            ((a,b) => ((a===1)&&(b>a))? col[j]=dem: ((b!==1)&&(b!==a))? ERROR.NO_SOLUTIONS(j,i,b,a):{}) (col[j],dem)  ))
 
         let makeMagic = (funArgs, shape, l=0) => {
             if (shape.length === 0) return this.fun(...funArgs)
             let newArgs = []
             for (let j=0;j<shape[0];j++) newArgs[j] = args.map((a,i)=> 
-                (maskShapes[i][l] === 0)? funArgs[i] : (maskShapes[i][l] === 1)? funArgs[i][0] : funArgs[i][j]  )
+                (sadowShapes[i][l] === 0)? funArgs[i] : (sadowShapes[i][l] === 1)? funArgs[i][0] : funArgs[i][j]  )
             return newArgs.map(a => makeMagic(a, shape.slice(1)),l++ )
         }
-
         return makeMagic(args,rezShape)
     }
 }
@@ -93,25 +71,32 @@ let bad = [[[1,1],[2,2]],[[3,3],[4,4],[5,5]]] // пример поломанно
 // Тестовые ф-и
 // ф-я одного аргумента нулевой размерности (число), возводит в квадрат
 let sqrZ = new Zipper((x)=>x**2,[0])
-console.log('sqrZ (',x,') = ', sqrZ(x))
+// console.log('sqrZ (',x,') = ', sqrZ(x))
 
 // ф-я concat, от 2 аргументов, размерности 1 (вектор)
 let concatZ =  new Zipper((a,b)=>a.concat(b),[1,1])
-console.log('concatZ =',concatZ(x,y))
+// console.log('concatZ =',concatZ(x,y))
 
 
 let concat3 =  new Zipper((a,b,c)=>a.concat(b).concat(c), [1,1,1] )
-console.log('concat3 = ',concat3(9,x,y))
+// console.log('concat3 = ',concat3(9,x,y))
+let cc = 1
+let a = testTen([1,2,3,4],()=>1)
+let b = testTen([3,4],()=>2)
+let c = testTen([3,1],()=>{cc++;return cc})
+console.log('concat3 = ',concat3(a,b,c))
 
 // размерность может быть разной 
 let fun3 =  new Zipper((a,b,c)=> [a,b[0],c[0][0]], [0,1,2] )
-console.log('fun3 = ',fun3(3,y,x))
+// console.log('fun3 = ',fun3(3,y,x))
+
+
 
 // можно стыковать
 // console.log('sqrZ(fun3.. = ',sqrZ(fun3(3,y,x)))
 
 // можно использовать внутри
 let sqrZ2 = new Zipper((x)=>sqrZ(x+1),[0])
-console.log('sqrZ2 (',x,') = ', sqrZ2(x))
+// console.log('sqrZ2 (',x,') = ', sqrZ2(x))
 
 
