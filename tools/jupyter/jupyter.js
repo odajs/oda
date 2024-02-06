61c0fa321e3915cee094fb6cebc8566816e602c2
ilysaz <ilysaz@gmail.com>
2024-02-06 16:00:49 +0300

    AutoCommit: 81040

--- tools/jupyter/jupyter.js
+++ tools/jupyter/jupyter.js
@@ -1,20 +1,16 @@
-const path = import.meta.url.split('/').slice(0, -1).join('/');
-
 ODA({ is: 'oda-jupyter', imports: '@oda/button',
     template: `
         <style>
             :host{
                 @apply --vertical;
                 @apply --flex;
-                padding: 8px;
-                height: calc(100% - 18px);
-                overflow-x: auto;
+                padding: 12px 6px;
+                height: calc(100% - 24px);
             }
         </style>
-        <div ~if="readOnly || !isReady" style="height: 16px;"></div>
-        <oda-jupyter-divider ~if="!readOnly && isReady" index="-1" style="padding: 6px 0 2px; 0" :focused="!notebook?.cells?.length"></oda-jupyter-divider>
+        <oda-jupyter-divider index="-1" :focused="!notebook?.cells?.length"></oda-jupyter-divider>
         <div ~for="notebook?.cells" class="vertical no-flex">
-            <oda-jupyter-cell :cell="$for.item" :focused="focusedIndex === $for.index" @tap.stop="focusedIndex = (readOnly ? -1 : $for.index)"></oda-jupyter-cell>
+            <oda-jupyter-cell :cell="$for.item" :focused="focusedIndex === $for.index" @tap.stop="focusedIndex = (_readOnly ? -1 : $for.index)"></oda-jupyter-cell>
             <oda-jupyter-divider :index="$for.index" style="margin-top: 4px;"></oda-jupyter-divider>
         </div>
     `,
@@ -23,38 +19,23 @@ ODA({ is: 'oda-jupyter', imports: '@oda/button',
     },
     $public: {
         $pdp: true,
-        readOnly: {
-            $def: false,
-            get() {
-                return this.notebook?.readOnly;
-            }
-        },
-        collapsedMode: false,
         iconSize: 24,
-        editors: ['Код', 'Текст']
-    },
-    focusedIndex: {
-        $pdp: true,
-        $def: -1,
-    },
-    get focusedItem() { return this.notebook?.cells?.[this.focusedIndex] || undefined },
-    set src(n) {
-        if (n) {
-            if (!n.startsWith('http'))
-                n = path + '/' + n;
-            ODA.loadJSON(n).then(res => {
-                this.notebook = res;
-            })
+        editors: ['Код', 'Текст'],
+        readOnly: false,
+        get _readOnly() {
+            return this.notebook?.readOnly || this.readOnly;
         }
     },
     notebook: {
         $pdp: true,
         $def: null
     },
-    isReady: false,
-    attached() {
-        this.async(() => this.isReady = true, 300);
-    }
+    focusedIndex: {
+        $pdp: true,
+        $def: -1,
+    },
+    get focusedItem() { return this.notebook?.cells?.[this.focusedIndex] || undefined }
+
 })
 
 ODA({ is: 'oda-jupyter-divider',
@@ -87,16 +68,10 @@ ODA({ is: 'oda-jupyter-divider',
                 padding: 0px 4px 0px 0px;
                 border-radius: 4px;
             }
-            div {
-                opacity: 1;
-                height: 10px;
-                align-items: center;
-                justify-content: start;
-            }
         </style>
         <div class="horizontal center">
-            <div ~if="index!==-1" style="width: 100%; position: absolute; top: 2px; height: 1px; border-bottom: 2px solid gray;"></div>
-            <oda-button :icon-size icon="icons:add" ~for="editors" @tap.stop="addCell($for.item)">{{$for.item}}</oda-button>
+            <div ~if="!_readOnly && index!==-1" style="width: 100%; position: absolute; top: 2px; height: 1px; border-bottom: 2px solid gray;"></div>
+            <oda-button ~if="!_readOnly" :icon-size icon="icons:add" ~for="editors" @tap.stop="addCell($for.item)">{{$for.item}}</oda-button>
         </div>
     `,
     index: -1,
@@ -104,10 +79,8 @@ ODA({ is: 'oda-jupyter-divider',
         this.focusedIndex = -1;
         this.notebook ||= {};
         this.notebook.cells ||= [];
-        this.notebook.cells.splice(1 + (+this.index), 0, { cell_type, source: cell_type + '...' });
-        this.async(() => {
-            this.focusedIndex = 1 + (+this.index);
-        })
+        this.notebook.cells.splice(1 + (+this.index), 0, { cell_type });
+        this.async(() => this.focusedIndex = 1 + (+this.index));
     }
 })
 
@@ -115,38 +88,17 @@ ODA({ is: 'oda-jupyter-cell',
     template: `
         <style>
             :host {
-                position: relative;
                 @apply --no-flex;
+                position: relative;
                 padding: 1px;
-                height: {{editMode ? '80vh' : ''}};
                 margin: 0 2px;
-                opacity: 0;
                 transition: opacity ease-out 1s;
             }
-            .main {
-                min-height: 22px;
-            }
-            .editor{
-                height: {{editMode ? '80vh' : ((cell?.enableResize || control?.enableResize) && cell?.cell_h) ? cell.cell_h + 'px' : '100%'}};
-                min-height: {{editMode ? '80vh' : ''}};
-                max-height: {{editMode ? '80vh' : ''}};
-            }
-            .row {
-                cursor: pointer;
-                padding: 5px;
-                color: gray;
-                font-size: 12px;
-                border: 1px solid lightgray;
-            }
         </style>
-        <div class="vertical flex main" ~if="!collapsedMode || (collapsedMode && editMode)" style="height: 100%;">
-            <div class="vertical flex main">
-                <h3 ~if="cell?.structure?.label">{{cell?.structure?.label}}</h3>
-                <div ~if="isReady" ~is="cell?.cell_extType || cellType" class="editor" ~class="{shadow: !readOnly && focused}" :edit-mode="!readOnly && focused && editMode" :source="cell.source || ''"  :src="cell.source || ''" :args="cell.args" :fount="cell.fount" :label="cell.label" @change="onchange" :syspanel="!readOnly && focused && editMode" :read-only></div>
-            </div>
+        <div class="vertical flex main" style="height: 100%; min-height: 22px;">
+            <div ~is="this.cellType || 'div'" :cell ~class="{shadow: !_readOnly && focused}" :edit-mode></div>
         </div>
-        <div class="row" ~if="collapsedMode && !editMode" ~class="{shadow: !readOnly && focused}">{{cell?.label || cell?.cell_type || ''}}</div>
-        <oda-jupyter-toolbar ~if="!readOnly && focused" ~style="{top: '-' + iconSize + 'px'}"></oda-jupyter-toolbar>
+        <oda-jupyter-toolbar ~if="!_readOnly && focused" ~style="{top: '-' + (iconSize - 4) + 'px'}"></oda-jupyter-toolbar>
     `,
     set cell(n) {
         if (n) {
@@ -154,46 +106,10 @@ ODA({ is: 'oda-jupyter-cell',
             this.cellType = 'oda-jupyter-' + (type === 'Код' ? 'code' : 'text')  + '-editor';
         }
     },
-    get control() {
-        return this.$(this.cellType);
-    },
     focused: false,
     editMode: {
         $pdp: true,
         $def: false,
-    },
-    cellType: 'div',
-    $observers: [
-        function _focused(focused) {
-            if (focused) {
-                this.focusedNode = this.cell.structure;
-            }
-            if (!focused) this.editMode = false;
-        }
-    ],
-    $listeners: {
-        endSplitterMove(e) {
-            if (!this.readOnly) {
-                if (e.detail.value.direction === 'horizontal') {
-                    this.cell.cell_h = e.detail.value.h;
-                    this.cell.cell_h = this.cell.cell_h < 20 ? 20 : this.cell.cell_h;
-                    this.cell.enableResize = true;
-                    this.async(() => this.$('.main').style.height = 'unset', 100);
-                }
-            }
-        }
-    },
-    onchange(e) {
-        if (this.isReady) return;
-        this.cell.source = e.detail?.value;
-        this.fire('change', this.cell.source);
-    },
-    isReady: false,
-    attached() {
-        this.async(() => {
-            this.isReady = true;
-            this.async(() => { this.style.opacity = 1 }, 300);
-        }, 100);
     }
 })
 
@@ -216,61 +132,12 @@ ODA({ is: 'oda-jupyter-toolbar',
         <oda-button :disabled="focusedIndex === 0" :icon-size icon="icons:arrow-back:90" @tap="moveCell(-1)"></oda-button>
         <oda-button :disabled="focusedIndex >= notebook?.cells?.length - 1" :icon-size icon="icons:arrow-back:270" @tap="moveCell(1)"></oda-button>
         <span style="width: 8px"></span>
-        <oda-button :icon-size icon="icons:settings" ~show="enableSettings" @tap="showSettings"></oda-button>
         <oda-button :icon-size icon="icons:delete" @tap="deleteCell"></oda-button>
         <span style="width: 8px"></span>
         <oda-button allow-toggle ::toggled="editMode" :icon-size :icon="editMode?'icons:close':'editor:mode-edit'" @tap="editMode = !editMode"> </oda-button>
     `,
-    enableSettings() {
-        return Object.keys(this.control?.props || {}).length > 0;
-    },
-    enableSettings2() {
-        return Object.keys(this.control?.usedControl?.props || {}).length > 0;
-    },
     iconSize: 20,
     cell: null,
-    async showSettings(e) {
-        if (!this.enableSettings()) return;
-        let control = this.control;
-        let io = {};
-        let props = {};
-        Object.keys(control.props).forEach(key => {
-            io[key] = control[key];
-            props[key] = {
-                default: control[key],
-                type: typeof control[key],
-                list: control.props[key].list || [],
-                category: 'cell - ' + control.localName
-            }
-        })
-        let control2;
-        if (this.enableSettings2()) {
-            control2 = control.usedControl;
-            Object.keys(control2.props).forEach(key => {
-                io[key] = control2[key];
-                props[key] = {
-                    default: control2[key],
-                    type: typeof control2[key],
-                    list: control2.props?.[key]?.list || [],
-                    category: 'editor - ' + control2.localName
-                }
-            })
-        }
-        io.props = props;
-        io = new Proxy(io, {
-            set: function (target, key, value) {
-                if (target.props[key].category === 'cell - ' + control.localName) {
-                    control[key] = value;
-                    control.controlSetArgs?.({ key, value, setArgs: true });
-                } else if (target.props[key].category === 'editor - ' + control2.localName) {
-                    control2[key] = value;
-                    control.controlSetArgs?.({ key, value, setArgs: false });
-                }
-                target[key] = value;
-                return true;
-            }
-        })
-    },
     moveCell(v) {
         this.editMode = false;
         let idx = this.focusedIndex;
