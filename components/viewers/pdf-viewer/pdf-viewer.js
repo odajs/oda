126fc505dffbddb6daab286b6c4fc81c0bd44e28
ilysaz <ilysaz@gmail.com>
2024-05-17 11:00:14 +0300

    AutoCommit: 81918

--- components/viewers/pdf-viewer/pdf-viewer.js
+++ components/viewers/pdf-viewer/pdf-viewer.js
@@ -1,3 +1,5 @@
+const PATH = import.meta.url.replace('pdf-viewer.js','');
+
 ODA({ is: 'oda-pdf-viewer', imports: '@oda/button',
     template: `
         <style>
@@ -7,56 +9,13 @@ ODA({ is: 'oda-pdf-viewer', imports: '@oda/button',
                 position: relative;
             }
         </style>
-        <iframe style="width: 100%; height: 100%; border: none;"></iframe>
-        <oda-button icon="icons:fullscreen" fill="lightgray" icon-size="26" @tap="setFullscreen" style="position: absolute; top: 10px; right: -8px; z-index: 9999"></oda-button>
+        <iframe :src style="width: 100%; height: 100%; border: none;"></iframe>
     `,
-    url: '',
-    file: undefined,
-    isReady: false,
-    attached() {
-        this.isReady = true;
-    },
-    $observers: {
-        urlChanged(url, isReady) {
-            if (isReady)
-                this.$('iframe').src = url || '';
-        },
-        fileChanged(file, isReady) {
-            if (file && isReady) {
-                const reader = new FileReader();
-                reader.onload = async (e) => {
-                    const buffer = await e.target.result;
-                    this.getUrl(buffer);
-                }
-                reader.readAsArrayBuffer(file);
-            }
+    url: {
+        $def: '',
+        set(n) {
+            this.src = PATH + 'web/viewer.html?file=' + (n?.startsWith('./') ? PATH + n.replace('./', '') : n);
         }
     },
-    async getUrl(buffer) {
-        const fileBlob = await new Blob([new Uint8Array(buffer)], { type: 'application/pdf'})
-        this.url = URL.createObjectURL(fileBlob);
-    },
-    setFullscreen() {
-        this.fullscreenMode = !this.fullscreenMode;
-        const element = this;
-        if (this.fullscreenMode) {
-            if (element.requestFullscreen) {
-                element.requestFullscreen();
-            } else if (element.mozRequestFullScreen) {
-                element.mozRequestFullScreen();
-            } else if (element.webkitRequestFullscreen) {
-                element.webkitRequestFullscreen();
-            } else if (element.msRequestFullscreen) {
-                element.msRequestFullscreen();
-            }
-        } else {
-            if (document.exitFullscreen) {
-                document.exitFullscreen();
-            } else if (document.mozCancelFullScreen) {
-                document.mozCancelFullScreen();
-            } else if (document.webkitExitFullscreen) {
-                document.webkitExitFullscreen();
-            }
-        }
-    }
+    src: ''
 })
