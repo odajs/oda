433c587a6cfa1539ab830b4c38a1cd02056384f3
ilysaz <ilysaz@gmail.com>
2024-06-19 16:00:10 +0300

    AutoCommit: 82296

--- tools/jupyter/jupyter-tree/jupyter-tree.js
+++ tools/jupyter/jupyter-tree/jupyter-tree.js
@@ -5,6 +5,14 @@ ODA({ is: 'oda-jupyter-tree', extends: 'oda-tree',
     get dataSet() {
         return this.notebook?.items
     },
+    onTapRows(e) {
+        this.$super('oda-table', 'onTapRows', e);
+        // const selector = `oda-jupyter-cell[id="${e.target.item.id}"]`;
+        // this.jupyter.$(selector)?.scrollIntoView();
+        const cellElements = this.jupyter.$$('oda-jupyter-cell');
+        const cellElement = cellElements.find(el => el.cell.id === e.target.item.id);
+        cellElement?.scrollIntoView();
+    },
     attached() {
         // this.jupyter.addEventListener('changed', (e) => {
         //     this.async(()=>{ this.items = undefined; });
