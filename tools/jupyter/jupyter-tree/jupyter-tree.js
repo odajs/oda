706d8fd1001e97d3945cc937f812bc443b751c92
ilysaz <ilysaz@gmail.com>
2024-06-21 13:30:11 +0300

    AutoCommit: 82324

--- tools/jupyter/jupyter-tree/jupyter-tree.js
+++ tools/jupyter/jupyter-tree/jupyter-tree.js
@@ -5,6 +5,7 @@ ODA({ is: 'oda-jupyter-tree', extends: 'oda-tree',
     get dataSet() {
         return this.notebook?.items
     },
+    cellTemplate: 'oda-jupyter-tree-cell',
     onTapRows(e) {
         this.$super('oda-table', 'onTapRows', e);
         // const selector = `oda-jupyter-cell[id="${e.target.item.id}"]`;
@@ -14,3 +15,40 @@ ODA({ is: 'oda-jupyter-tree', extends: 'oda-tree',
         cellElement?.scrollIntoView();
     }
  });
+ODA({
+    is: 'oda-jupyter-tree-cell', extends: 'this, oda-table-cell',
+    template: /*html*/`
+        <style>
+            span{
+                padding: 0px 8px;
+            }
+            oda-icon{
+                transform: scale(.7);
+            }
+        </style>
+        <oda-icon ~if="!bold" :icon></oda-icon>
+    `,
+    bold:{
+        $attr: true,
+        get(){
+            return !!this.item?.h
+        }
+    },
+    header:{
+        $attr: true,
+        get(){
+            return !!this.item?.h
+        }
+    },
+    get icon(){
+        switch (this.item.type){
+            case 'code':
+                return 'av:play-circle-outline';
+            case 'html':
+                return 'box:i-code-alt';
+        }
+        if (this.item.h)
+            return 'bootstrap:type-h'+this.item.h
+        return 'bootstrap:text-left';
+    }
+})
