11d8935884b787c2bb04e7b9301444c5cbd4ebd2
ilysaz <ilysaz@gmail.com>
2024-07-16 16:00:11 +0300

    AutoCommit: 83115

--- components/viewers/error-viewer/error-viewer.js
+++ components/viewers/error-viewer/error-viewer.js
@@ -1,16 +1,28 @@
 ODA({
     is: 'oda-error-viewer', imports: '@oda/ace-editor',
     template: /*html*/`
-    <style>:host{@apply --vertical; }</style>
-    <div style="margin-bottom: 8px;align-items: center; white-space: break-spaces;" horizontal>
+    <style>
+        :host{
+            @apply --vertical;
+        }
+        :host .message-block{
+            margin-bottom: 8px;
+            align-items: center;
+            white-space: break-spaces;
+            padding: 8px;
+        }
+    </style>
+    <div class="error message-block" horizontal>
         <div>{{message}}</div>
         <oda-button @tap="_gotoError(message)" icon="enterprise:target"></oda-button>
     </div>
-    <oda-ace-editor :wrap="false" :value="code" :mode></oda-ace-editor>
+    <oda-ace-editor class="flex" :wrap="false" :value="code" :mode read-only></oda-ace-editor>
     `,
     message: '',
     set code(code) {
-        this.async(() => this._gotoError(this.message), 100)
+        if (code) {
+            this.async(() => this._gotoError(this.message), 100)
+        }
     },
     mode: 'xquery',
     _gotoError(message) {
