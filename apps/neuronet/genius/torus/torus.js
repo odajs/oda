f2f1fa6352d5edd644a099d8813b7a73d2513d2b
ilysaz <ilysaz@gmail.com>
2024-07-29 12:00:15 +0300

    AutoCommit: 83407

--- apps/neuronet/genius/torus/torus.js
+++ apps/neuronet/genius/torus/torus.js
@@ -284,18 +284,18 @@ export class tensor{
                 return r + Math.abs(v);
             })/this.grad.length
             for(let i = 0; i<bins.length; i++){
-                let g = this.grad[i];
+                let g = this.grad[i] * tensor.LEARNING_RATE;
                 let sign = Math.sign(g)
                 let value = +bins[i];
                 switch (sign){
                     case 1: //g>0
-                        // if(!value && Math.max(0,Math.min(1,(g + 1)/2))>Math.random())
-                        if(!value && g > mean)
+                        if(!value && Math.max(0,Math.min(1,(g + 1)/2))>Math.random())
+                        // if(!value && g > mean)
                            value = 1
                         break;
                     case -1: //g<0
-                        if(value && -g > mean)
-                        // if(value && Math.max(0,Math.min(1,(g + 1)/2))>Math.random())
+                        // if(value && -g > mean)
+                        if(value && Math.max(0,Math.min(1,(g + 1)/2))>Math.random())
                            value = 0
                         break;
                 }
@@ -1025,8 +1025,8 @@ tensor.prototype.softmax = function (){
             let sum = data.reduce((r, sj, j)=>{
                 let v = (i === j) ?d * (1 - d): -d * sj;
                 return r + v
-            })// * out.grad[i];
-            this.grad[i] += sum * out.grad[i] /tensor.GRADIENT_DIVIDER;
+            })
+            this.grad[i] += sum * out.grad[i] / tensor.GRADIENT_DIVIDER;
         }
     }
     return out;
