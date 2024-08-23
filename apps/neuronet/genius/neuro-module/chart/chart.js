73e9fd646c34023669732f6c03141fe6f4825f65
ilysaz <ilysaz@gmail.com>
2024-08-23 12:00:49 +0300

    AutoCommit: 83876

---
+++ apps/neuronet/genius/neuro-module/chart/chart.js
@@ -0,0 +1,45 @@
+ODA({is: 'oda-chart', template: /*html*/ `
+    <h3 ~if='label.length>0'>{{label}}</h3>
+    <svg class='box' :width :height xmlns='http://www.w3.org/2000/svg' :viewBox="'0 0 ' + width + ' ' + height">
+        <rect x="0" y="0" :width :height :fill='background'/>
+        <path :d='pathD' fill="none" :stroke :stroke-width="strokeWidth"/>
+    </svg>`,
+    label:'',
+    width:1000,
+    height:300,
+    maxPoint:100,
+    stroke:"#070637",
+    strokeWidth:2,
+    background:'#a9caff',
+
+    get pathD(){
+        if (this.pointsL.length<2) return ''
+        let padding = Math.min(this.width,this.height)/10
+        let [w,h] = [this.width,this.height].map(x=>x-padding*2)
+        let dx = w/(this.pointsL.length-1)
+        let [min,max] = [Math.min(...this.pointsL),Math.max(...this.pointsL)]
+        let points = this.pointsL.map((v,i)=>[i*dx+padding,h-(v-min)/(max-min)*h+padding])
+        let rez = 'M ' + points[0].join(' ') + ' '
+        if (this.bezier) rez+=  'Q ' + points[0].join(' ') + ', ' + points[1].join(' ') + ' '
+        else  rez += 'L ' + points[1].join(' ') + ' '
+        points.slice(2).forEach(e => rez += (this.bezier?'T ':'L ') + e.join(' ') + ' ' )
+        return rez
+    },
+    get pointsL() {
+        let [lL,lP] = [this.dataSet.length,this.maxPoint]
+        if (lL<lP) return this.dataSet
+        let newP = Array(lP-1).fill(0).map((_,i)=>{
+            let idx = Math.floor(i*(lL/(lP-1)))
+            let dL = Math.floor(lL/(lP-1))+1
+            return this.dataSet.slice(idx,idx+dL).reduce((a,c)=>a+c)/dL
+        })
+        newP.push(this.dataSet[lL-1])
+        return newP
+    },
+    bezier:false,
+
+    dataSet:{
+        $def: new Array(20000).fill(0).map(_=> Math.random()*5-2)
+    },
+
+})
