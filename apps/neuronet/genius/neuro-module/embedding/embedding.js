9b37f70867fac6fa7be6ed5a67d15193e87ba424
ilysaz <ilysaz@gmail.com>
2024-08-23 11:30:37 +0300

    AutoCommit: 83875

---
+++ apps/neuronet/genius/neuro-module/embedding/embedding.js
@@ -0,0 +1,53 @@
+ODA({is: 'oda-embedding',
+    template: `
+        <style>
+            :host{
+                @apply --vertical;
+                @apply --dark;
+                overflow: hidden;
+                max-height: 50%;
+                max-width: 50%;
+            }
+            span{
+                padding: 4px;
+                width: 150px;
+                font-size: small;
+
+            }
+        </style>
+        <div flex style="overflow-y: auto;">
+            <div stadow ~for="tokenizer?.vocabulary" vertical>
+                <div class="horizontal">
+                    <span no-flex>{{$for.item.id}}: {{$for.key}}</span>
+                    <div class="flex" ~style="getBackGradient($for.item.emb.data)"></div>
+                    <span style="text-align: right;" no-flex>{{(+$for.item.error).toLocaleString('ru-RU', {style: 'percent',  minimumFractionDigits: 2, maximumFractionDigits: 2})}}</span>
+                </div>
+            </div>
+        </div>
+        <div class="horizontal">
+            <span>tokens: {{size}}</span>
+            <span>error:  {{tokenizer?.error.toLocaleString('ru-RU', {style: 'percent',  minimumFractionDigits: 2, maximumFractionDigits: 2})}}</span>
+        </div>
+
+
+    `,
+    tokenizer: null,
+    get error(){
+        return this.tokenizer?.error;
+    },
+    get size(){
+        return this.tokenizer?.size;
+    },
+    getBackGradient(vector){
+        return {background: `linear-gradient(to right, ${this.getColors(vector)})`}
+    },
+    getColors(items){
+        const getColor = (val)=>{
+            return Math.round(300 * val);
+        }
+        const length = items.length
+        return  Array.from(items || []).map((val, idx, items)=>{
+            return `hsl(${getColor(val)}, 100%, 50%) ${((idx+1)/length) * 100}%, hsl(${getColor(items[idx+1] || 0)}, 100%, 50%)  ${((idx+1)/length) * 100}%`;
+        }).join(', ');
+    },
+})
