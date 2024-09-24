b4c820cb7a18f00f7abc4598122798831abf694c
ilysaz <ilysaz@gmail.com>
2024-09-24 12:00:09 +0300

    AutoCommit: 84772

--- apps/neuronet/genius/neuro-module/loss-chart/loss-chart.js
+++ apps/neuronet/genius/neuro-module/loss-chart/loss-chart.js
@@ -1,17 +1,29 @@
 ODA({is: 'oda-loss-chart', template: /*html*/ `
     <style>
         :host {
+            @apply --vertical;
             @apply --flex;
+            @apply --dark;
         }
+        h3 {padding-left:40px;}
+        .small {font-size:7px; font-family: monospace;}
     </style>
-    <h3 ~if='label.length>0'>{{label}}</h3>
-    <svg width='100%' height='100%' xmlns='http://www.w3.org/2000/svg' :view-box="'0 0 ' + width + ' ' + height" preserveAspectRatio="none">
+    <h3  ~if='label.length>0'>{{label}}</h3>
+    <svg xmlns='http://www.w3.org/2000/svg' :view-box="'0 0 ' + width + ' ' + height" preserveAspectRatio="none">
         <rect x="0" y="0" :width :height fill='var(--dark-background)'/>
         <path ~for='pointsL' ~if='kvo>1' :d='pathD($for.item)' fill="none" :stroke='lineColor($for.index)' stroke-width="2"/>
-    </svg>`,
+        <path id='qqq' ~for='granLine' ~if='kvo>1' :d='$for.item' fill="none" stroke='#fff' stroke-width="0.2"/>
+        <text :x="padding-17" :y="padding-1" class='small' >max: {{minMax[1]}}</text>
+        <text :x="padding-17" :y="height-padding+7" class='small' >min: {{minMax[0]}}</text>
+    </svg>
+    <ul class='legend' >
+        <li ~for='pointsL' ~style='"color:"+lineColor($for.index)+";"'> {{legendText($for.index)}} </li>
+    </li>
+    `,
+
     data: [],
-    width:1000,
-    height:300,
+    width:600,
+    height:400,
     $public: {
         label:'',
         strokeWidth:{
@@ -21,8 +33,12 @@ ODA({is: 'oda-loss-chart', template: /*html*/ `
             }
         },
         bezier:false,
+        legend:[],
     },
-
+    get granLine() {
+        let [p, h, w] = [this.padding, this.height, this.width]
+        return [[0,p,w,p], [0,h-p,w,h-p], [p,0,p,h], [w-p,0,w-p,h] ].map(([x1,y1,x2,y2])=>`M ${x1} ${y1} L ${x2} ${y2}`)
+    },
 
     get kvo() {return this.pointsL.length},
 
@@ -30,12 +46,15 @@ ODA({is: 'oda-loss-chart', template: /*html*/ `
         let ollP = this.data.flat()
         return  [Math.min(...ollP),Math.max(...ollP)]
     },
-    get padding() {return Math.min(this.width,this.height)/10},
+    get padding() {return Math.min(this.width,this.height)/15},
     get wh() { return [this.width,this.height].map(x=>x-this.padding*2) },
 
     lineColor(i) {
         let c = Math.floor(360/this.kvo*i)+100
         return `hsl(${c}, 100%, 80%)`
+    },
+    legendText(i) {
+        return this.legend[i] || `line № ${i}`
     },
 
     pathD(ps){
