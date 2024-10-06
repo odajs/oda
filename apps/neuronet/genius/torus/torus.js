21a0f8b64a5d346f845000527e9a25c65af2f353
ilysaz <ilysaz@gmail.com>
2024-10-06 21:00:08 +0300

    AutoCommit: 84917

--- apps/neuronet/genius/torus/torus.js
+++ apps/neuronet/genius/torus/torus.js
@@ -936,11 +936,9 @@ tensor.prototype.crossEntropy = function (target) {
     let idx = -1;
     for(let i = 0; i<this.size; i+=step){
         const slice = this.data.slice(i, i + step);
-        errs[++idx] = -slice.data.reduce((r, x, i)=>r + y[i] * Math.log(x), 0);
+        errs[++idx] = -slice.reduce((r, x, i)=>r + y[i] * Math.log(x), 0);
     }
-
-    let error = -this.data.reduce((r, x, i)=>r + y[i] * Math.log(x), 0);
-    error /= this.size  // todo дополнительные измерения
+    let error = errs.reduce((r,v)=>r+v)/errs.length;
     const out = tensor.from([error])._src(this)._label('crossEntropy');
     this._back = ()=>{
         this.src.forEach(src=>{
