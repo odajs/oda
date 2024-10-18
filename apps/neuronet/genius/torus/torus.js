6ff2945a33e23e527db62ed9b6d612157b2734ec
ilysaz <ilysaz@gmail.com>
2024-10-19 00:00:18 +0300

    AutoCommit: 85370

--- apps/neuronet/genius/torus/torus.js
+++ apps/neuronet/genius/torus/torus.js
@@ -1041,7 +1041,7 @@ tensor.prototype.view = function (...shape) {
         shape = shape[0];
     if(Object.equal(shape[0]?.constructor, tensor))
         shape = shape[0].shape;
-    const out =  tensor.from(this.data).reshape(shape)._src(this);
+    const out =  tensor.from(this.data).reshape(shape)._src(this)._label('view');
     out._back = ()=>{
         for (let i = 0; i < this.grad.length; i++){
             this.grad[i] += out.grad[i];
