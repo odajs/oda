89dccb539f264f2691fa67703756aaf54f260de1
ilysaz <ilysaz@gmail.com>
2024-10-19 13:30:07 +0300

    AutoCommit: 85387

--- apps/neuronet/genius/neuro-module/neuro-module.js
+++ apps/neuronet/genius/neuro-module/neuro-module.js
@@ -252,7 +252,7 @@ class Embedding extends Linear{
         })
         input = tensor.from(data).reshape(...input.shape, this.size_in);
         let output = super.forward(input);
-        output = output.softmax(-1);
+        // output = output.softmax(-1);
         return output;
     }
 }
