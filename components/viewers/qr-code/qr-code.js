b41680cf601deec4316e48d2dea350fc24e04b8f
ilysaz <ilysaz@gmail.com>
2024-12-10 15:30:09 +0300

    AutoCommit: 87138

--- components/viewers/qr-code/qr-code.js
+++ components/viewers/qr-code/qr-code.js
@@ -1,7 +1,8 @@
 import './lib/qr.js';
-ODA({ is: 'oda-qr-code',
+ODA({
+    is: 'oda-qr-code',
     template: `
-        <qr-code :data="value" :margin :modulesize="moduleSize" :format></qr-code>
+        <qr-code @tap='save' :data="value" :margin :modulesize="moduleSize" :format></qr-code>
     `,
     $public: {
         value: '',
@@ -10,6 +11,30 @@ ODA({ is: 'oda-qr-code',
             $list: ['png', 'svg', 'html'],
         },
         margin: 2,
-        moduleSize: 5
+        moduleSize: 5,
+        saveFile:false,
+    },
+    get source() {
+        let url = el => {
+            let rowStr = (new XMLSerializer()).serializeToString(el);
+            let [bn,mime,end] = (this.format === 'svg')? ['<?xml version="1.0" encoding="UTF-8" standalone="no"?>', "image/svg+xml;",'']
+                              : (this.format === 'html')? ['<!DOCTYPE html><html><head><meta charset="utf-8"></head><body>',"text/html;",'</body></html>']
+                              : ['','','']
+            return URL.createObjectURL(new Blob([bn,rowStr,end], {type: mime + "charset=utf-8" }));
+        }
+        return (this.format === 'png')? this.$('qr-code').shadowRoot.firstChild.src
+             : (this.format === 'svg')? url( this.$('qr-code').shadowRoot.firstChild )
+             : (this.format === 'html')? url (this.$('qr-code').shadowRoot.firstChild.firstChild)
+             : undefined
+    },
+    save() {
+        if (!this.saveFile) return
+        const a = document.createElement('a');
+        a.href = this.source;
+        a.setAttribute('download', 'qr-code.'+this.format);
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+        console.log(111)
     }
 })
