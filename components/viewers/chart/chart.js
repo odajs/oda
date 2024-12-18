49f0636c8d49c98f22623ba76f7a4e7309e67da9
ilysaz <ilysaz@gmail.com>
2024-12-18 17:00:09 +0300

    AutoCommit: 87426

--- components/viewers/chart/chart.js
+++ components/viewers/chart/chart.js
@@ -24,7 +24,7 @@ ODA({ is: 'oda-chart', imports: './lib/chart.js',
         async chartChanged(type, data, options) {
             const canvas = this.$?.('canvas');
             if (type && data && options && canvas) {
-                this.chart?.destroy() && this.chart.destroy();
+                this.chart?.destroy();
                 const ctx = canvas.getContext('2d');
                 this.chart = new Chart(ctx, { type: this.type, data: this.data, options: { ...this.defaultOptions, ...this.options } });
             }
