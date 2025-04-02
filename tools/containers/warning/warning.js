840f8862cb1ec377566fca56a65fed0805bbdbfe
ilysaz <ilysaz@gmail.com>
2025-04-02 12:00:08 +0300

    AutoCommit: 88210

--- tools/containers/warning/warning.js
+++ tools/containers/warning/warning.js
@@ -1,20 +1,20 @@
 import '../dialog/dialog.js';
-ODA({is:'oda-warning', extends: 'oda-dialog',
+ODA({is: 'oda-warning', extends: 'oda-dialog',
     title: 'Warning',
     icon: 'icons:warning'
 })
 const warning = ODA.showWarning;
-ODA.showWarning = async (text, params={})=>{
+ODA.showWarning = async (text, params = {}) => {
     text = text.toString();
     params.hideCancelButton = true;
     let error = false;
-    if (text.startsWith('Error: ')){
+    if (text.startsWith('Error: ')) {
         error = text.startsWith('Error:');
         text = text.replace('Error: ', '')
     }
-    params.icon ??= error?'icons:error':'icons:warning';
-    if (error){
-        params.title = 'Error'
+    params.icon ??= error ? 'icons:error' : 'icons:warning';
+    if (error) {
+        params.title ??= 'Error'
     }
-    return await warning('oda-dialog-message', {icon: params.icon, message: text, error: error, warning: !error}, params);
+    return await warning('oda-dialog-message', { icon: params.icon, message: text, error: error, warning: !error }, params);
 }
