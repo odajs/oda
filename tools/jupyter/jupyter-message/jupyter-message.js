3824dfbd680a513020defc45a80fabb04291a6d5
ilysaz <ilysaz@gmail.com>
2026-05-25 15:00:09 +0300

    AutoCommit: 92446

--- tools/jupyter/jupyter-message/jupyter-message.js
+++ tools/jupyter/jupyter-message/jupyter-message.js
@@ -1,4 +1,4 @@
-ODA({ is: 'oda-jupyter-message', imports: '@oda/markdown-viewer',
+ODA({ is: 'oda-jupyter-message', imports: '@oda/markdown-viewer, @oda/button',
     icon: 'icons:error-outline',
     template: /*html*/`
         <style>
@@ -7,12 +7,16 @@ ODA({ is: 'oda-jupyter-message', imports: '@oda/markdown-viewer',
                 @apply --flex;
                 position: relative;
             }
+            oda-button {
+                border-bottom: 1px solid var(--dark-1);
+            }
             oda-markdown-viewer {
                 padding: 8px;
                 height: 0;
                 overflow: auto;
             }
         </style>
+        <oda-button @tap="message=''">Clear</oda-button>
         <oda-markdown-viewer flex :value="message"></oda-markdown-viewer>
     `,
     message: ''
